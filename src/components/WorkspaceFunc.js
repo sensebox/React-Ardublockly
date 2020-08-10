08dab82d002353f2f1d54d6b652ce1bcc597f86d
Delucse <46593742+Delucse@users.noreply.github.com>
2020-08-10 10:06:34 +0200

    showing CircularProgress while compiling

--- src/components/WorkspaceFunc.js
+++ src/components/WorkspaceFunc.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import MaxBlocks from './MaxBlocks';
+import Compile from './Compile';
 
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -30,29 +31,6 @@ class WorkspaceFunc extends Component {
     this.setState({ open: !this.state });
   }
 
-  compile = () => {
-    const data = {
-      "board": process.env.REACT_APP_BOARD,
-      "sketch": this.props.arduino
-    };
-
-    fetch(`${process.env.REACT_APP_COMPILER_URL}/compile`, {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        this.download(data.data.id)
-      });
-  }
-
-  download = (id) => {
-    const filename = 'sketch'
-    window.open(`${process.env.REACT_APP_COMPILER_URL}/download?id=${id}&board=${process.env.REACT_APP_BOARD}&filename=${filename}`, '_self');
-  }
-
   render() {
     return (
       <div style={{ marginTop: '20px' }}>
@@ -74,9 +52,7 @@ class WorkspaceFunc extends Component {
           Get XML Code
         </Button>
         <MaxBlocks />
-        <Button style={{ float: 'right', color: 'white' }} variant="contained" color="primary" onClick={() => this.compile()}>
-          Compile
-        </Button>
+        <Compile />
       </div>
     );
   };
