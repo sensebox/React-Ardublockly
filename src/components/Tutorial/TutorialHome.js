ab95f9eee6b19ea2d51a5b85f59ac270040097d1
Delucse <46593742+Delucse@users.noreply.github.com>
2020-08-31 13:15:50 +0200

    overview of tutorials

--- src/components/Tutorial/TutorialHome.js
+++ src/components/Tutorial/TutorialHome.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
+import tutorials from './tutorials.json';
+
+import { Link } from 'react-router-dom';
+
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 class TutorialHome extends Component {
   render() {
     return (
-      <h1>Tutorial Home</h1>
+      <div>
+        <h1>Tutorial-Übersicht</h1>
+        <Grid container spacing={2}>
+          {tutorials.map((tutorial, i) => (
+            <Grid item xs={12} sm={6} md={4} xl={3} key={i}>
+              <Link to={`/tutorial/${i+1}`} style={{textDecoration: 'none', color: 'inherit'}}>
+                <Paper style={{height: '150px', padding: '10px'}}>Tutorial {i+1}</Paper>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     );
   };
 }
