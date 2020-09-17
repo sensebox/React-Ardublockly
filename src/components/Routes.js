1cf1f0fd628aa5bace9a243a8f467179610dcdb8
Delucse <46593742+Delucse@users.noreply.github.com>
2020-09-17 18:55:51 +0200

    add id and title

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Tutorial from './Tutorial/Tutorial';
 import TutorialHome from './Tutorial/TutorialHome';
+import Builder from './Tutorial/Builder/Builder';
 import NotFound from './NotFound';
 
 class Routes extends Component {
@@ -15,6 +16,7 @@ class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/tutorial" exact component={TutorialHome} />
+          <Route path="/tutorial/builder" exact component={Builder}/>
           <Route path="/tutorial/:tutorialId" exact component={Tutorial} />
           <Route component={NotFound} />
         </Switch>
