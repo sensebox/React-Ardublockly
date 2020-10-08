08ee8b3b5c49b6273942cb9ce13cb73e1b8bebfc
Mario <mario.pesch@uni-muenster.de>
2020-10-08 16:25:37 +0200

    add display tutorial

--- src/data/tutorials.js
+++ src/data/tutorials.js
@@ -5,5 +5,6 @@ let tutorials = [
     require('./loops_01.json'),
     require('./Wenn-DannBedingungen.json'),
     require('./variablen_01.json'),
+    require('./AnzeigeVonMesswertenAufDemDisplay.json')
 ]
 module.exports = tutorials;
