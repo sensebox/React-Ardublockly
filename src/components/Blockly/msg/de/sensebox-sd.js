47632d14223a52b7a86730ebdd64ee9fd52fe880
Mario <mario.pesch@uni-muenster.de>
2021-01-13 15:37:41 +0100

    add missing imports

--- src/components/Blockly/msg/de/sensebox-sd.js
+++ src/components/Blockly/msg/de/sensebox-sd.js
@@ -2,12 +2,12 @@ export const SD = {
     /**
      * SD-Block
      */
-    senseBox_sd_create_file = "Erstelle Datei auf SD-Karte",
-    senseBox_sd_write_file = "Schreibe Daten auf SD-Karte",
-    senseBox_sd_open_file = "Öffne eine Datei auf der SD-Karte",
-    senseBox_sd_create_file_tooltip = "Erstellt eine Datei auf der Karte. Stecke das SD-Bee auf den Steckplatz **XBEE2**. Die **maximale** Länge des Dateinamen sind **8 Zeichen**. Die Datei sollte zuerst im *Setup()* erstellt werden",
-    senseBox_sd_write_file_tooptip = "Schreibe Daten auf die SD-Karte. Beachte, dass die Datei zuerst geöffnet werden muss.",
-    senseBox_sd_open_file_tooltip = "Öffne die Datei auf der SD-Karte, um Dateien zu speichern. Am Ende der Schleife wird die Datei automatisch wieder geschlossen.",
-    sensebox_sd_filename = "Daten",
-    senseBox_sd_decimals = "Dezimalen",
+    senseBox_sd_create_file: "Erstelle Datei auf SD-Karte",
+    senseBox_sd_write_file: "Schreibe Daten auf SD-Karte",
+    senseBox_sd_open_file: "Öffne eine Datei auf der SD-Karte",
+    senseBox_sd_create_file_tooltip: "Erstellt eine Datei auf der Karte. Stecke das SD-Bee auf den Steckplatz **XBEE2**. Die **maximale** Länge des Dateinamen sind **8 Zeichen**. Die Datei sollte zuerst im *Setup()* erstellt werden",
+    senseBox_sd_write_file_tooptip: "Schreibe Daten auf die SD-Karte. Beachte, dass die Datei zuerst geöffnet werden muss.",
+    senseBox_sd_open_file_tooltip: "Öffne die Datei auf der SD-Karte, um Dateien zu speichern. Am Ende der Schleife wird die Datei automatisch wieder geschlossen.",
+    sensebox_sd_filename: "Daten",
+    senseBox_sd_decimals: "Dezimalen",
 }
