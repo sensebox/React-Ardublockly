59267ca5333f031b7a8e448ce6bc6410ea9cd9f2
Mario Pesch <mario.pesch@uni-muenster.de>
2021-03-30 15:03:34 +0200

    add multiplexer
    closes #81

--- src/components/Blockly/msg/de.js
+++ src/components/Blockly/msg/de.js
@@ -1,48 +1,49 @@
-
-import { AUDIO } from './de/audio';
-import { FAQ } from './de/faq';
-import { IO } from './de/io';
-import { LOGIC } from './de/logic';
-import { LOOPS } from './de/loops';
-import { MATH } from './de/math';
-import { MQTT } from './de/mqtt';
-import { DISPLAY } from './de/sensebox-display';
-import { LED } from './de/sensebox-led';
-import { LORA } from './de/sensebox-lora';
-import { OSEM } from './de/sensebox-osem';
-import { SD } from './de/sensebox-sd';
-import { SENSORS } from './de/sensebox-sensors';
-import { TELEGRAM } from './de/sensebox-telegram';
-import { WEB } from './de/sensebox-web';
-import { TEXT } from './de/text';
-import { TIME } from './de/time';
-import { TOURS } from './de/tours';
-import { TRANSLATIONS } from './de/translations';
-import { UI } from './de/ui';
-import { VARIABLES } from './de/variables';
-import { WEBSERVER } from './de/webserver';
+import { AUDIO } from "./de/audio";
+import { FAQ } from "./de/faq";
+import { IO } from "./de/io";
+import { LOGIC } from "./de/logic";
+import { LOOPS } from "./de/loops";
+import { MATH } from "./de/math";
+import { MQTT } from "./de/mqtt";
+import { DISPLAY } from "./de/sensebox-display";
+import { LED } from "./de/sensebox-led";
+import { LORA } from "./de/sensebox-lora";
+import { OSEM } from "./de/sensebox-osem";
+import { SD } from "./de/sensebox-sd";
+import { SENSORS } from "./de/sensebox-sensors";
+import { SENSEBOX } from "./de/sensebox";
+import { TELEGRAM } from "./de/sensebox-telegram";
+import { WEB } from "./de/sensebox-web";
+import { TEXT } from "./de/text";
+import { TIME } from "./de/time";
+import { TOURS } from "./de/tours";
+import { TRANSLATIONS } from "./de/translations";
+import { UI } from "./de/ui";
+import { VARIABLES } from "./de/variables";
+import { WEBSERVER } from "./de/webserver";
 
 export const De = {
-    ...AUDIO,
-    ...FAQ,
-    ...IO,
-    ...LOGIC,
-    ...LOOPS,
-    ...MATH,
-    ...MQTT,
-    ...DISPLAY,
-    ...LED,
-    ...LORA,
-    ...OSEM,
-    ...SD,
-    ...SENSORS,
-    ...TELEGRAM,
-    ...WEB,
-    ...TEXT,
-    ...TIME,
-    ...TOURS,
-    ...TRANSLATIONS,
-    ...UI,
-    ...VARIABLES,
-    ...WEBSERVER
-}
+  ...AUDIO,
+  ...FAQ,
+  ...IO,
+  ...LOGIC,
+  ...LOOPS,
+  ...MATH,
+  ...MQTT,
+  ...DISPLAY,
+  ...LED,
+  ...LORA,
+  ...OSEM,
+  ...SD,
+  ...SENSORS,
+  ...SENSEBOX,
+  ...TELEGRAM,
+  ...WEB,
+  ...TEXT,
+  ...TIME,
+  ...TOURS,
+  ...TRANSLATIONS,
+  ...UI,
+  ...VARIABLES,
+  ...WEBSERVER,
+};
