c140b78e3f1a5c60f118ef22951f6aab98ecbb21
Mario Pesch <mario.pesch@uni-muenster.de>
2021-05-06 15:21:54 +0200

    add first phyphox blocks

---
+++ src/components/Blockly/msg/de/sensebox-ble.js
@@ -0,0 +1,12 @@
+export const BLE = {
+  /**
+   * Phyphox Init
+   */
+  sensebox_phyphox_init: "Phyphox Experiment",
+  sensebox_phyphox_createExperiment: "Erstelle Experiment",
+  sensebox_phyphox_experimentName: "Name des Experiments",
+  sensebox_phyphox_experimentTitle: "Titel",
+  sensebox_phyphox_experimentCategory: "Kategorie",
+  sensebox_phyphox_experimentDescription: "Beschreibung",
+  sensebox_phyphox_writeValues: "Sende Werte",
+};
