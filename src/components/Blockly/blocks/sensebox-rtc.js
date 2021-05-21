2ced9a55b534c3d5b6d5a38caed4620b107bee21
Mario Pesch <mario.pesch@uni-muenster.de>
2021-05-21 11:28:35 +0200

    add rtc blocks

---
+++ src/components/Blockly/blocks/sensebox-rtc.js
@@ -0,0 +1,79 @@
+import * as Blockly from "blockly";
+import { getColour } from "../helpers/colour";
+import { selectedBoard } from "../helpers/board";
+import * as Types from "../helpers/types";
+
+Blockly.Blocks["sensebox_rtc_init"] = {
+  init: function () {
+    this.setHelpUrl(Blockly.Msg.sensebox_rtc_helpurl);
+    this.setColour(getColour().time);
+    this.appendDummyInput().appendField(Blockly.Msg.sensebox_rtc_init);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip(Blockly.Msg.sensebox_rtc_init_tooltip);
+  },
+};
+
+Blockly.Blocks["sensebox_rtc_set"] = {
+  init: function () {
+    this.setHelpUrl(Blockly.Msg.sensebox_rtc_helpurl);
+    this.setColour(getColour().time);
+    this.appendDummyInput().appendField(Blockly.Msg.sensebox_rtc_set);
+    this.appendValueInput("second").appendField(
+      Blockly.Msg.sensebox_rtc_second
+    );
+    this.appendValueInput("minutes").appendField(
+      Blockly.Msg.sensebox_rtc_minutes
+    );
+    this.appendValueInput("hour").appendField(Blockly.Msg.sensebox_rtc_hour);
+    this.appendValueInput("day").appendField(Blockly.Msg.sensebox_rtc_day);
+    this.appendValueInput("month").appendField(Blockly.Msg.sensebox_rtc_month);
+    this.appendValueInput("year").appendField(Blockly.Msg.sensebox_rtc_year);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip(Blockly.Msg.sensebox_rtc_set_tooltip);
+  },
+};
+
+Blockly.Blocks["sensebox_rtc_set_ntp"] = {
+  init: function () {
+    this.setHelpUrl(Blockly.Msg.sensebox_rtc_helpurl);
+    this.setColour(getColour().time);
+    this.appendValueInput("time").appendField(Blockly.Msg.sensebox_rtc_set_ntp);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip(Blockly.Msg.sensebox_rtc_set_ntp_tooltip);
+  },
+};
+
+Blockly.Blocks["sensebox_rtc_get"] = {
+  init: function () {
+    this.setHelpUrl(Blockly.Msg.sensebox_rtc_helpurl);
+    this.setColour(getColour().time);
+    this.appendDummyInput()
+      .appendField(Blockly.Msg.sensebox_rtc_get)
+      .appendField(
+        new Blockly.FieldDropdown([
+          ["hour", "hour"],
+          ["min", "minutes"],
+          ["sec", "seconds"],
+          ["day", "day"],
+          ["month", "month"],
+          ["year", "year"],
+        ]),
+        "dropdown"
+      );
+    this.setOutput(true, Types.LARGE_NUMBER.typeId);
+    this.setTooltip(Blockly.Msg.sensebox_rtc_get_tooltip);
+  },
+};
+
+Blockly.Blocks["sensebox_rtc_get_timestamp"] = {
+  init: function () {
+    this.setHelpUrl(Blockly.Msg.sensebox_rtc_helpurl);
+    this.setColour(getColour().time);
+    this.appendDummyInput().appendField(Blockly.Msg.sensebox_rtc_get_timestamp);
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg.sensebox_rtc_get_timestamp_tooltip);
+  },
+};
---
+++ src/components/Blockly/msg/de/sensebox-rtc.js
@@ -0,0 +1,6 @@
+export const RTC = {
+  sensebox_rtc_init: "Initialisiere RTC",
+  sensebox_rtc_set: "Set Time",
+  sensebox_rtc_get_timestamp: "Get Timestamp",
+  sensebox_rtc_get: "Get: ",
+};
