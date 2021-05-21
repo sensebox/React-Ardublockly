b6c90772ed105d1ac828ef3f9831c83ec15d5e5e
Mario Pesch <mario.pesch@uni-muenster.de>
2021-05-21 11:28:35 +0200

    add rtc blocks

---
+++ src/components/Blockly/msg/en/sensebox-rtc.js
@@ -0,0 +1,13 @@
+export const RTC = {
+  sensebox_rtc_init: "Initialisiere RTC",
+  sensebox_rtc_set: "Set Time manually",
+  sensebox_rtc_set_ntp: "Set time via NTP-Server",
+  sensebox_rtc_get_timestamp: "Get Timestamp",
+  sensebox_rtc_get: "Get: ",
+  sensebox_rtc_second: "seconds",
+  sensebox_rtc_minutes: "minutes",
+  sensebox_rtc_hour: "hour",
+  sensebox_rtc_day: "day",
+  sensebox_rtc_month: "month",
+  sensebox_rtc_year: "year",
+};
