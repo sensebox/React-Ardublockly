d67cb74401d3a064b1f1be8b4c08bfb42d53cc41
Mario Pesch <mario.pesch@uni-muenster.de>
2021-11-03 12:22:51 +0100

    add translations and informations

--- src/components/Workspace/Compile.js
+++ src/components/Workspace/Compile.js
@@ -22,7 +22,6 @@ import "prismjs/plugins/line-numbers/prism-line-numbers";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import MuiDrawer from "@material-ui/core/Drawer";
 import Dialog from "../Dialog";
-import { Link } from "react-router-dom";
 
 const styles = (theme) => ({
   backdrop: {
