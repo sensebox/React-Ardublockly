ab0915aac98bb4c96ec0e1dc37e8419388ad8d94
Mario Pesch <mario.pesch@uni-muenster.de>
2022-01-25 19:39:23 +0100

    add translations

--- src/components/Blockly/msg/en/sensebox-web.js
+++ src/components/Blockly/msg/en/sensebox-web.js
@@ -1,11 +1,24 @@
 export const WEB = {
-    /**
-     * WiFi
-     */
-    senseBox_wifi_connect: "Connect to Wifi",
-    senseBox_wifi_ssid: "Networkname",
-    senseBox_wifi_tooltip: "Connects to a wireless network. If you want to connect to an unsecured network (e.g. Freifunk) clear the password field. The WiFi-Bee must be plugged into the **XBEE1** slot.",
-    senseBox_wifi_startap: "Initialize Wifi Access Point",
-    senseBox_wifi_startap_tooltip: "Creates a WiFi access point to which you can connect. Note that only 1 device can be connected at a time.",
-    senseBox_wifi_helpurl: "https://en.docs.sensebox.de/blockly/blockly-web-wifi/",
-}
+  /**
+   * WiFi
+   */
+  senseBox_wifi_connect: "Connect to Wifi",
+  senseBox_wifi_ssid: "Networkname",
+  senseBox_wifi_tooltip:
+    "Connects to a wireless network. If you want to connect to an unsecured network (e.g. Freifunk) clear the password field. The WiFi-Bee must be plugged into the **XBEE1** slot.",
+  senseBox_wifi_startap: "Initialize Wifi Access Point",
+  senseBox_wifi_startap_tooltip:
+    "Creates a WiFi access point to which you can connect. Note that only 1 device can be connected at a time.",
+  senseBox_wifi_helpurl:
+    "https://en.docs.sensebox.de/blockly/blockly-web-wifi/",
+  senseBox_ethernet: "Connect to Ethernet",
+  senseBox_ethernet_dhcp: "DHCP",
+  senseBox_ethernet_manuel_config: "Manual configuration",
+  senseBox_ethernet_ip: "IP address",
+  senseBox_ethernet_gateway: "Gateway",
+  senseBox_ethernet_subnetmask: "Subnet mask",
+  senseBox_ethernet_mac: "MAC address",
+  senseBox_ethernet_dns: "DNS-Server",
+  senseBox_ethernet_tooltip:
+    "Connect the LAN-Bee to the XBEE1 slot. Most networks use DHCP and assign an IP address automatically. If you want to do a manual configuration select **Manual Configuration** in the dropdown menu and enter the appropriate data.",
+};
