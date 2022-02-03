80811fa573257c67d650c4b529f707362784f8f1
Mario Pesch <mario.pesch@uni-muenster.de>
2022-02-03 17:09:06 +0100

    add multi interval feature
    closes #74

--- src/components/Blockly/msg/en/time.js
+++ src/components/Blockly/msg/en/time.js
@@ -1,17 +1,19 @@
 export const TIME = {
-
-    senseBox_interval: "ms",
-    senseBox_interval_timer: "Measuring interval",
-    senseBox_interval_timer_tip: "Setup an Intervall",
-    ARD_TIME_DELAY: "wait",
-    ARD_TIME_DELAY_MICROS: "microseconds",
-    ARD_TIME_DELAY_MICRO_TIP: "Wait specific time in microseconds",
-    ARD_TIME_DELAY_TIP: "Wait specific time in milliseconds",
-    ARD_TIME_INF: "wait forever (end program)",
-    ARD_TIME_INF_TIP: "Wait indefinitely, stopping the program.",
-    ARD_TIME_MICROS: "current elapsed Time (microseconds)",
-    ARD_TIME_MICROS_TIP: "Returns the number of microseconds since the Arduino board began running the current program. Has to be stored in a positive long integer",
-    ARD_TIME_MILLIS: "current elapsed Time (milliseconds)",
-    ARD_TIME_MILLIS_TIP: "Returns the number of milliseconds since the Arduino board began running the current program. Has to be stored in a positive long integer",
-    ARD_TIME_MS: "milliseconds",
-}
+  senseBox_interval: "ms",
+  senseBox_interval_timer: "Interval: ",
+  senseBox_interval_timer_tip:
+    "Define an interval to be executed every x milliseconds",
+  ARD_TIME_DELAY: "wait",
+  ARD_TIME_DELAY_MICROS: "microseconds",
+  ARD_TIME_DELAY_MICRO_TIP: "Wait specific time in microseconds",
+  ARD_TIME_DELAY_TIP: "Wait specific time in milliseconds",
+  ARD_TIME_INF: "wait forever (end program)",
+  ARD_TIME_INF_TIP: "Wait indefinitely, stopping the program.",
+  ARD_TIME_MICROS: "current elapsed Time (microseconds)",
+  ARD_TIME_MICROS_TIP:
+    "Returns the number of microseconds since the Arduino board began running the current program. Has to be stored in a positive long integer",
+  ARD_TIME_MILLIS: "current elapsed Time (milliseconds)",
+  ARD_TIME_MILLIS_TIP:
+    "Returns the number of milliseconds since the Arduino board began running the current program. Has to be stored in a positive long integer",
+  ARD_TIME_MS: "milliseconds",
+};
