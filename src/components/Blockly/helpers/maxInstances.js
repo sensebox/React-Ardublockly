4cece2783075ae9c33909611823078512f587b6b
Mario Pesch <mario.pesch@uni-muenster.de>
2022-02-08 11:03:03 +0100

    remove interval from maxInstances

--- src/components/Blockly/helpers/maxInstances.js
+++ src/components/Blockly/helpers/maxInstances.js
@@ -9,7 +9,6 @@ const maxInstances = {
   sensebox_display_beginDisplay: 1,
   sensebox_telegram: 1,
   sensebox_telegram_do: 1,
-  sensebox_interval_timer: 1,
   sensebox_osem_connection: 1,
   sensebox_lora_initialize_otaa: 1,
   sensebox_lora_initialize_abp: 1,
