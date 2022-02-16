772797bb6b5e0d702ad97039e3b1ad9bb59da1de
Mario Pesch <mario.pesch@uni-muenster.de>
2022-02-16 20:01:22 +0100

    add restart to osem block

--- src/components/Blockly/msg/de/sensebox-osem.js
+++ src/components/Blockly/msg/de/sensebox-osem.js
@@ -1,17 +1,20 @@
 export const OSEM = {
-    /**
-     * openSenseMap
-     */
-    senseBox_osem_connection_tip: "stellt eine WLAN verbindung her",
-    senseBox_send_to_osem_tip: "sende Messwert an",
-    senseBox_send_to_osem: "Sende Messwert an die openSenseMap",
-    senseBox_osem_connection: "Verbinde mit openSenseMap:",
-    senseBox_osem_host: "opensensemap.org",
-    senseBox_osem_host_workshop: "workshop.opensensemap.org",
-    senseBox_osem_exposure: "Typ",
-    senseBox_osem_stationary: "Stationär",
-    senseBox_osem_mobile: "Mobil",
-    senseBox_osem_access_token: "API Schlüssel",
-    senseBox_send_mobile_to_osem: "Sende Messwert und Standort an die openSenseMap",
-    senseBox_send_mobile_to_osem_tip: "Sende Messwert und Standort an die openSenseMap",
-}
+  /**
+   * openSenseMap
+   */
+  senseBox_osem_connection_tip: "stellt eine WLAN verbindung her",
+  senseBox_send_to_osem_tip: "sende Messwert an",
+  senseBox_send_to_osem: "Sende Messwert an die openSenseMap",
+  senseBox_osem_connection: "Verbinde mit openSenseMap:",
+  senseBox_osem_host: "opensensemap.org",
+  senseBox_osem_host_workshop: "workshop.opensensemap.org",
+  senseBox_osem_exposure: "Typ",
+  senseBox_osem_stationary: "Stationär",
+  senseBox_osem_mobile: "Mobil",
+  senseBox_osem_access_token: "API Schlüssel",
+  senseBox_send_mobile_to_osem:
+    "Sende Messwert und Standort an die openSenseMap",
+  senseBox_send_mobile_to_osem_tip:
+    "Sende Messwert und Standort an die openSenseMap",
+  senseBox_osem_restart: "Bei Verbindungsverlust Gerät neustarten?",
+};
