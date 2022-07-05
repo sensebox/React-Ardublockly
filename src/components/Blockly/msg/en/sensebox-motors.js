dd8278193934289d5726ccf88b8a0e7c81609b61
Bjoern <bjoernluig@gmail.com>
2022-07-05 23:05:54 +0200

    added blocks for simple stepper motor

---
+++ src/components/Blockly/msg/en/sensebox-motors.js
@@ -0,0 +1,18 @@
+export const MOTORS = {
+
+    /**
+     * Motors
+     */
+
+    sensebox_motors_beginStepperMotor: "Initialize Stepper Motor (28BYJ-48)",
+    sensebox_motors_beginStepperMotor_rpm: "Rotations per Minute",
+    sensebox_motors_beginStepperMotor_pins: "Input Pins",
+    sensebox_motors_beginStepperMotor_tooltip: "TODO",
+    sensebox_motors_beginStepperMotor_helpurl: "TODO",
+    sensebox_motors_beginStepperMotor_step: "TODO",
+
+    sensebox_motors_moveStepperMotor: "Move Stepper Motor (28BYJ-48)",
+    sensebox_motors_moveStepperMotor_step: "Steps:",
+    sensebox_motors_moveStepperMotor_tooltip: "TODO",
+    sensebox_motors_moveStepperMotor_helpurl: "TODO",
+}
