047588842c567d91207f52b688056f26ba90dd15
Bjoern Luig <bjoernluigqgmail.com>
2022-07-30 23:32:22 +0200

    added german messages and put only servo motor in toolbox

--- src/components/Blockly/blocks/sensebox-motors.js
+++ src/components/Blockly/blocks/sensebox-motors.js
@@ -4,52 +4,6 @@ import { selectedBoard } from "../helpers/board";
 import { FieldSlider } from "@blockly/field-slider";
 
 /**
- * Stepper Motor
- * 
- */
-Blockly.Blocks["sensebox_motors_beginStepperMotor"] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor);
-        this.appendDummyInput()
-            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor_pins);
-        this.appendDummyInput()
-            .setAlign(Blockly.ALIGN_RIGHT)
-            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in1")
-            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in2")
-            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in3")
-            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in4");
-        this.setFieldValue("1", "in1");
-        this.setFieldValue("2", "in2");
-        this.setFieldValue("3", "in3");
-        this.setFieldValue("4", "in4");
-        this.appendDummyInput()
-            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor_rpm)
-            .appendField(new FieldSlider(10, 1, 15), "rpm");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(getColour().sensebox);
-        this.setTooltip(Blockly.Msg.sensebox_motors_beginStepperMotor_tooltip);
-        this.setHelpUrl(Blockly.Msg.sensebox_motors_beginStepperMotor_helpurl);
-    },
-};
-
-Blockly.Blocks["sensebox_motors_moveStepperMotor"] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField(Blockly.Msg.sensebox_motors_moveStepperMotor);
-        this.appendValueInput("steps", "Number")
-            .setAlign(Blockly.ALIGN_RIGHT)
-            .appendField(Blockly.Msg.sensebox_motors_moveStepperMotor_step);
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(getColour().sensebox);
-        this.setTooltip(Blockly.Msg.sensebox_motors_moveStepperMotor_tooltip);
-        this.setHelpUrl(Blockly.Msg.sensebox_motors_moveStepperMotor_helpurl);
-    },
-};
-
-/**
  * Servo Motor
  * 
  */
@@ -83,8 +37,8 @@ Blockly.Blocks["sensebox_motors_moveServoMotor"] = {
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(getColour().sensebox);
-        this.setTooltip(Blockly.Msg.sensebox_motors_moveStepperMotor_tooltip);
-        this.setHelpUrl(Blockly.Msg.sensebox_motors_moveStepperMotor_helpurl);
+        this.setTooltip(Blockly.Msg.sensebox_motors_moveServoMotor_tooltip);
+        this.setHelpUrl(Blockly.Msg.sensebox_motors_moveServoMotor_helpurl);
     },
 };
 
@@ -139,5 +93,52 @@ Blockly.Blocks["sensebox_motors_I2CMotorBoard_stopDCMotor"] = {
         this.setColour(getColour().sensebox);
         this.setTooltip(Blockly.Msg.sensebox_motors_I2CMotorBoard_stopDCMotor_tooltip);
         this.setHelpUrl(Blockly.Msg.sensebox_motors_I2CMotorBoard_stopDCMotor_helpurl);
+    },
+};
+
+
+/**
+ * Stepper Motor
+ * 
+ */
+Blockly.Blocks["sensebox_motors_beginStepperMotor"] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor);
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor_pins);
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in1")
+            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in2")
+            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in3")
+            .appendField(new Blockly.FieldDropdown(selectedBoard().digitalPins), "in4");
+        this.setFieldValue("1", "in1");
+        this.setFieldValue("2", "in2");
+        this.setFieldValue("3", "in3");
+        this.setFieldValue("4", "in4");
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.sensebox_motors_beginStepperMotor_rpm)
+            .appendField(new FieldSlider(3, 1, 3), "rpm");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(getColour().sensebox);
+        this.setTooltip(Blockly.Msg.sensebox_motors_beginStepperMotor_tooltip);
+        this.setHelpUrl(Blockly.Msg.sensebox_motors_beginStepperMotor_helpurl);
+    },
+};
+
+Blockly.Blocks["sensebox_motors_moveStepperMotor"] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.sensebox_motors_moveStepperMotor);
+        this.appendValueInput("steps", "Number")
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField(Blockly.Msg.sensebox_motors_moveStepperMotor_step);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(getColour().sensebox);
+        this.setTooltip(Blockly.Msg.sensebox_motors_moveStepperMotor_tooltip);
+        this.setHelpUrl(Blockly.Msg.sensebox_motors_moveStepperMotor_helpurl);
     },
 };
