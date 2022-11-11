f95b1b36ca5e35fea64161674484e756d3e8e338
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-11-11 12:21:13 +0100

    fix faq video

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import * as Blockly from "blockly";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import Container from "@material-ui/core/Container";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
@@ -61,6 +62,7 @@ class Faq extends Component {
                         className="news"
                         allowDangerousHtml="true"
                         children={object.answer}
+                        rehypePlugins={[rehypeRaw]}
                       ></ReactMarkdown>
                     </Typography>
                   </ExpansionPanelDetails>
