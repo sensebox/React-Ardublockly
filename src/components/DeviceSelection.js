2de54c0e9bf8466325e72139135e85cb8f95ee59
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-11-15 16:30:13 +0100

    rewrite board check

--- src/components/DeviceSelection.js
+++ src/components/DeviceSelection.js
@@ -29,7 +29,7 @@ class DeviceSeclection extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
+      open: props.selectedBoard ? false : true,
       selectedBoard: "",
       saveSettings: false,
 
@@ -58,39 +58,34 @@ class DeviceSeclection extends Component {
   };
 
   render() {
-    const board = this.props.selectedBoard;
-    if (board) {
-      return null;
-    }
-    else {
-      return (
-        <Dialog
-          style={{ zIndex: 9999999 }}
-          fullWidth
-          maxWidth={"xl"}
-          open={this.state.open}
-          title={Blockly.Msg.deviceselection_head}
-          content={""}
-          onClick={this.toggleDialog}
-          disabled={this.state.selectedBoard === ""}
-        >
-          <div>
-            <Grid container spacing={2} style={{ textAlign: "center" }}>
-              <Grid item xs={6}>
-                <IconButton onClick={(e) => this.onclick(e, "mcu")}>
-                  <Avatar
-                    alt="Sensebox MCU"
-                    src="/media/hardware/senseboxmcu.png"
-                    style={{
-                      border: this.state.selectedBoard === "mcu" ? 'medium solid DeepSkyBlue' : "0.1px solid lightgray",
-                      width: "20vw",
-                      height: "20vw"
-                    }}
-                  />
-                </IconButton>
-                <p>senseBox MCU</p>
-              </Grid>
-              {/* <Grid item xs={4}>
+    return (
+      <Dialog
+        style={{ zIndex: 9999999 }}
+        fullWidth
+        maxWidth={"xl"}
+        open={this.state.open}
+        title={Blockly.Msg.deviceselection_head}
+        content={""}
+        onClick={this.toggleDialog}
+        disabled={this.state.selectedBoard === ""}
+      >
+        <div>
+          <Grid container spacing={2} style={{ textAlign: "center" }}>
+            <Grid item xs={6}>
+              <IconButton onClick={(e) => this.onclick(e, "mcu")}>
+                <Avatar
+                  alt="Sensebox MCU"
+                  src="/media/hardware/senseboxmcu.png"
+                  style={{
+                    border: this.state.selectedBoard === "mcu" ? 'medium solid DeepSkyBlue' : "0.1px solid lightgray",
+                    width: "20vw",
+                    height: "20vw"
+                  }}
+                />
+              </IconButton>
+              <p>senseBox MCU</p>
+            </Grid>
+            {/* <Grid item xs={4}>
             <IconButton onClick={(e) => this.onclick(e, "esp")}>
               <Avatar
                 alt="Sensebox ESP"
@@ -104,23 +99,23 @@ class DeviceSeclection extends Component {
               </IconButton>
               <p>Sensebox ESP</p>
             </Grid> */}
-              <Grid item xs={6}>
-                <IconButton onClick={(e) => this.onclick(e, "mini")}>
-                  <Avatar
-                    alt="Sensebox Mini"
-                    src="/media/hardware/senseboxmcumini.png"
-                    style={{
-                      border: this.state.selectedBoard === "mini" ? 'medium solid DeepSkyBlue' : "0.1px solid lightgray",
-                      width: "20vw",
-                      height: "20vw"
-                    }}
-                  />
-                </IconButton>
-                <p>senseBox MCU:mini</p>
-              </Grid>
+            <Grid item xs={6}>
+              <IconButton onClick={(e) => this.onclick(e, "mini")}>
+                <Avatar
+                  alt="Sensebox Mini"
+                  src="/media/hardware/senseboxmcumini.png"
+                  style={{
+                    border: this.state.selectedBoard === "mini" ? 'medium solid DeepSkyBlue' : "0.1px solid lightgray",
+                    width: "20vw",
+                    height: "20vw"
+                  }}
+                />
+              </IconButton>
+              <p>senseBox MCU:mini</p>
             </Grid>
-          </div>
-          {/* <FormControlLabel
+          </Grid>
+        </div>
+        {/* <FormControlLabel
           style={{ marginTop: "20px" }}
           classes={{ label: this.props.classes.label }}
           control={
@@ -135,12 +130,11 @@ class DeviceSeclection extends Component {
           }
           label={Blockly.Msg.deviceselection_keep_selection}
         /> */}
-          <Typography variant="body1" >
-            {Blockly.Msg.deviceselection_footnote} <a href="https://sensebox.github.io/blockly/">Arduino UNO</a> {Blockly.Msg.deviceselection_footnote_02} <a href="https://sensebox-blockly.netlify.app/ardublockly/?board=sensebox-mcu">senseBox MCU</a>
-          </Typography>
-        </Dialog>
-      );
-    }
+        <Typography variant="body1" >
+          {Blockly.Msg.deviceselection_footnote} <a href="https://sensebox.github.io/blockly/">Arduino UNO</a> {Blockly.Msg.deviceselection_footnote_02} <a href="https://sensebox-blockly.netlify.app/ardublockly/?board=sensebox-mcu">senseBox MCU</a>
+        </Typography>
+      </Dialog>
+    );
   }
 }
 
