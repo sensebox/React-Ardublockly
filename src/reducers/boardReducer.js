445553b88e4549a46a52decbbaf75c53f26e243d
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-11-16 11:44:16 +0100

    fix baground mcu image

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,11 +1,19 @@
 import { BOARD } from '../actions/types';
+import mini_opacity from "../data/mini_opacity.png"
+import mcu_opacity from "../data/mcu_opacity.png"
 
 const initialValue = () => {
   if (window.sessionStorage.getItem("board")) {
+    setBackgroundImage(window.sessionStorage.getItem("board"));
     return window.sessionStorage.getItem("board");
   }
   return null;
 };
+
+const setBackgroundImage = (board) => {
+  const root = document.querySelector(':root');
+  root.style.setProperty('--url', `url(${board === "mcu" ? mcu_opacity : mini_opacity})`);
+}
 
 const initialState = {
     board: initialValue()
