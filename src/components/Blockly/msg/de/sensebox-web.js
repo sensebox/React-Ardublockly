c8e6cb7d1fd1388a088ba399192cd3b60a6f784a
Mario Pesch <mario.pesch@uni-muenster.de>
2023-12-11 16:19:06 +0100

    add new blocks

--- src/components/Blockly/msg/de/sensebox-web.js
+++ src/components/Blockly/msg/de/sensebox-web.js
@@ -4,6 +4,8 @@ export const WEB = {
    */
   senseBox_wifi_connect: "Verbinde mit WLAN",
   senseBox_wifi_ssid: "Netzwerkname",
+  senseBox_wifi_user: "Benutzername",
+  senseBox_wifi_password: "Passwort",
   senseBox_wifi_ip: "IP-Address",
   senseBox_wifi_ip_tooltip: "Gibt die IP-Address der senseBox aus",
   senseBox_wifi_status: "WiFi-Status",
