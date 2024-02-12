7a8050c6b44f61d3589740ab6e02529442b13b22
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 10:15:37 +0100

    Update audio.js

--- src/components/Blockly/msg/hu/audio.js
+++ src/components/Blockly/msg/hu/audio.js
@@ -1,9 +1,9 @@
 export const AUDIO = {
 
-    ARD_TONEFREQ: "at frequency",
-    ARD_TONE_FREQ: "frequency",
+    ARD_TONEFREQ: "frekvencián",
+    ARD_TONE_FREQ: "frekvencia",
     ARD_TONE_PIN: "Tone PIN#",
-    ARD_TONE_PIN_TIP: "Generate audio tones on a pin",
-    ARD_TONE_TIP: "Sets tone on pin to specified frequency within range 31 - 65535 Hz",
-    ARD_TONE_WARNING: "Frequency must be in range 31 - 65535",
+    ARD_TONE_PIN_TIP: "Audió hangok generálása egy csapon",
+    ARD_TONE_TIP: "A hangot a tűn a megadott frekvenciára állítja 31 - 65535 Hz tartományban.",
+    ARD_TONE_WARNING: "A frekvenciának a 31-65535 tartományban kell lennie.",
 }
