86e00bd2bf2f8758844d41ad751694de1b425113
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 11:05:36 +0100

    Update tours.js

--- src/components/Blockly/msg/hu/tours.js
+++ src/components/Blockly/msg/hu/tours.js
@@ -2,23 +2,23 @@ export const TOURS = {
 
 
     /**
-     * Home Tour
+     * Otthoni túra
      */
-    hometour_wrapper: "Welcome to the new Learn- and Programmingapp for the senseBox. In this tour I will show you all the important features of the interface",
-    hometour_workspaceFunc: "Here you will find all the buttons to compile, save or share your program",
-    hometour_blocklyWindow: "This is your workspace. Here you can use the blocks to create your program code",
-    hometour_blocklyToolboxDiv: "The toolbox contains all the blocks. Connect them in the workspace and create your program code.",
-    hometour_compileBlocks: "When you are done programming and want to transfer your program code to the senseBox click here to compile your program code.",
-    hometour_menuButton: "In the menu you will find tutorials and a gallery with different example programs. Log in with your openSenseMap account and you will get even more features.",
-    hometour_showCode: "With this button you can show the generated program code",
-    hometour_shareBlocks: "Use this button to create a short link and share your blocks with other users",
+    hometour_wrapper: "Üdvözöljük a senseBox új tanulási és programozási alkalmazásán. Ebben a túrában a felület minden fontos funkcióját megmutatom neked",
+    hometour_workspaceFunc: "Itt találod az összes gombot a program összeállításához, mentéséhez vagy megosztásához",
+    hometour_blocklyWindow: "Ez az Ön munkaterülete. Itt használhatod a blokkokat a programkódod elkészítéséhez",
+    hometour_blocklyToolboxDiv: "Az eszköztár tartalmazza az összes blokkot. Csatlakoztassa őket a munkaterületen, és hozza létre a programkódját.",
+    hometour_compileBlocks: "Ha befejezted a programozást, és át akarod vinni a programkódodat a senseBoxba, kattints ide a programkód kompilálásához.",
+    hometour_menuButton: "A menüben találsz oktatóanyagokat és egy galériát különböző példaprogramokkal. Jelentkezz be az openSenseMap fiókoddal, és még több funkciót kapsz.",
+    hometour_showCode: "Ezzel a gombbal megjelenítheti a generált programkódot",
+    hometour_shareBlocks: "Ezzel a gombbal létrehozhat egy rövid linket, és megoszthatja blokkjait más felhasználókkal",
 
 
     /**
-     * Assessment Tour
+     * Értékelési túra
      */
 
-    assessmenttour_solutionCheck: "When your solution is ready click here to check it",
-    assessmenttour_assessmentDiv: "Let's go! Solve the following task to complete the tutorial.",
-    assessmenttour_injectionDiv: "Create your solution here. You can use any blocks from the toolbox.",
+    assessmenttour_solutionCheck: "Ha a megoldásod készen van, kattints ide az ellenőrzéshez",
+    assessmenttour_assessmentDiv: "Gyerünk! Oldja meg a következő feladatot a tananyag befejezéséhez.",
+    assessmenttour_injectionDiv: "Itt hozd létre a megoldásodat. Az eszköztárból bármelyik blokkot használhatod.",
 }
