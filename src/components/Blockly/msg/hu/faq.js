0074e2a58b82c8da97385bf55ea8f6ba59a2a549
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 10:16:27 +0100

    Update faq.js

--- src/components/Blockly/msg/hu/faq.js
+++ src/components/Blockly/msg/hu/faq.js
@@ -3,26 +3,26 @@ export const FAQ = {
    * FAQ
    */
 
-  faq_q1_question: `How can I copy my program to the senseBox?`,
-  faq_q1_answer: `To copy programs to the senseBox, connect it to the computer with the Micro USB cable. Then double click on the red reset button on the senseBox MCU. The senseBox will now be recognized as a removable disk on your computer and the previously created programs can be copied via drag & drop. After each change of the program code the program must be recompiled and transferred again.
-#### Activate learning mode of the MCU
-<iframe width="560" height="315" src="https://www.youtube.com/embed/jzlOJ7Zuqqw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  faq_q1_question: `Hogyan másolhatom a programomat a senseBox-ra?`,
+  faq_q1_answer: `A programok senseBox-ra történő másolásához csatlakoztassa a számítógéphez a Micro USB-kábellel. Ezután kattintson duplán a senseBox MCU piros reset gombjára. A senseBox most már cserélhető lemezként lesz felismerhető a számítógépen, és a korábban létrehozott programok drag & drop segítségével másolhatók. A programkód minden egyes módosítása után a programot újra kell fordítani és újra át kell vinni.
+#### Az MCU tanulási üzemmódjának aktiválása
+<iframe width="560" height="315" src="https://www.youtube.com/embed/jzlOJ7Zuqqw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; giroszkóp; picture-in-picture" allowfullscreen></iframe>
 
-#### Copying programs under macOS
-Copying programs under MacOS does not work via the Finder, but there are still two different ways to copy the programs:
-- [senseBox Copy Tool](https://sensebox.de/docs/senseBox_Sketch_Uploader_DE.zip)
+#### Programok másolása macOS alatt
+A programok másolása a MacOS alatt nem működik a Finderen keresztül, de a programok másolására mégis kétféle módon van lehetőség:
+- [senseBox másoló eszköz](https://sensebox.de/docs/senseBox_Sketch_Uploader_DE.zip)
 - [muCommander](https://www.mucommander.com/)
 `,
 
-  faq_q2_question: `With which senseBox is the programming environment compatible?`,
+  faq_q2_question: `Melyik senseBox-szal kompatibilis a programozási környezet?`,
   faq_q2_answer: `
-Basically the programming environment can be used with any senseBox with senseBox MCU. 
+Alapvetően a programozási környezet bármely senseBox MCU-val rendelkező senseBox MCU-val használható.
 `,
 
-  faq_q3_question: `I found an error or something is not working. Where can I report it?`,
+  faq_q3_question: `Találtam egy hibát, vagy valami nem működik. Hol jelenthetem?`,
   faq_q3_answer: `
-The best way to do this is to create an issue on [Github](https://github.com/sensebox/React-Ardublockly/issues). Alternatively you can send us an email to info(at)sensebox.de
+A legjobb módja ennek, ha létrehozol egy problémát a [Githubon](https://github.com/sensebox/React-Ardublockly/issues). Alternatívaként küldhet nekünk egy e-mailt az info(at)sensebox.de címre.
 `,
-  faq_tablet_question: `Can I program the senseBox with a tablet?`,
-  faq_tablet_answer: `Yes! Install the senseBox Connect app from the App Store and call up the learning and programming environment in your browser on the tablet. Detailed information on how to use the app can be found at [https://sensebox.de/app](https://sensebox.de/app)`,
+  faq_tablet_question: `Programozhatom a senseBoxot egy táblagéppel?`,
+  faq_tablet_answer: `Igen! Telepítse a senseBox Connect alkalmazást az App Store-ból, és hívja elő a tanulási és programozási környezetet a táblagép böngészőjében. Az alkalmazás használatára vonatkozó részletes információk a [https://sensebox.de/app](https://sensebox.de/app) oldalon találhatók.`,
 };
