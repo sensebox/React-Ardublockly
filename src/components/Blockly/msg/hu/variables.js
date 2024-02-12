e992dccdbbad2ca3d5f4da17c5927646a4da52e2
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 11:07:54 +0100

    Update variables.js

--- src/components/Blockly/msg/hu/variables.js
+++ src/components/Blockly/msg/hu/variables.js
@@ -1,12 +1,12 @@
 export const VARIABLES = {
     /**
-     * Typed Variable Modal
+     * Tipizált változó modális
      *
      */
 
 
     TYPED_VAR_MODAL_CONFIRM_BUTTON: "Ok",
-    TYPED_VAR_MODAL_VARIABLE_NAME_LABEL: "Variablen Name: ",
+    TYPED_VAR_MODAL_VARIABLE_NAME_LABEL: "Változó neve: ",
     TYPED_VAR_MODAL_TYPES_LABEL: "Variable Typen",
     TYPED_VAR_MODAL_CANCEL_BUTTON: "Abbrechen",
     TYPED_VAR_MODAL_TITLE: "Erstelle Variable",
@@ -15,10 +15,10 @@ export const VARIABLES = {
     VARIABLES_DEFAULT_NAME: "item",
     VARIABLES_GET_CREATE_SET: "Create 'set %1'",
     VARIABLES_GET_HELPURL: "https://github.com/google/blockly/wiki/Variables#get",
-    VARIABLES_GET_TOOLTIP: "Returns the value of this variable.",
+    VARIABLES_GET_TOOLTIP: "Visszaadja a változó értékét.",
     VARIABLES_SET: "set %1 to %2",
     VARIABLES_SET_CREATE_GET: "Create 'get %1'",
     VARIABLES_SET_HELPURL: "https://github.com/google/blockly/wiki/Variables#set",
-    VARIABLES_SET_TOOLTIP: "Sets this variable to be equal to the input.",
+    VARIABLES_SET_TOOLTIP: "Ezt a változót a bemeneti változóval egyenlővé teszi.",
 
 }
