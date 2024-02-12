0893a9c2ad50e5b49643bffb99666ce67f04cbd9
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 10:56:05 +0100

    Update sensebox-led.js

--- src/components/Blockly/msg/hu/sensebox-led.js
+++ src/components/Blockly/msg/hu/sensebox-led.js
@@ -1,6 +1,6 @@
 export const LED = {
-  senseBox_led: "LED connected to",
-  senseBox_led_tip: "simple LED. Don't forget the resistor",
+  senseBox_led: "LED csatlakoztatva",
+  senseBox_led_tip: "egyszerű LED. Ne felejtsd el az ellenállást",
 
   senseBox_rgb_led: "RGB-LED",
   senseBox_rgb_led_tip: "RGB-LED",
@@ -9,37 +9,37 @@ export const LED = {
    * WS2818 RGB LED
    */
   senseBox_ws2818_rgb_led: "Set RGB-LED at",
-  senseBox_ws2818_rgb_led_init: "Initialise RGB LED (WS2818)",
-  senseBox_ws2818_rgb_led_position: "Position",
-  senseBox_ws2818_rgb_led_brightness: "Brightness",
+  senseBox_ws2818_rgb_led_init: "RGB LED inicializálása (WS2818)",
+  senseBox_ws2818_rgb_led_position: "Pozíció",
+  senseBox_ws2818_rgb_led_brightness: "Brightness: Fényerő",
   senseBox_ws2818_rgb_led_tooltip:
-    "Change the color of your RGB LED with this block. Link a block for the color. If multiple RGB LEDs are chained together you can use the position to determine which LED is controlled.",
+    "Az RGB LED színének megváltoztatása ezzel a blokkal. Linkelje a színhez tartozó blokkot. Ha több RGB LED van egymáshoz láncolva, akkor a pozíció segítségével meghatározhatja, hogy melyik LED-et vezérli.",
   senseBox_ws2818_rgb_led_init_tooltip:
-    "Connect the RGB LED to one of the three **digital/analog ports**. If multiple RGB LEDs are daisy-chained together you can determine which LED is controlled by position.",
-  senseBox_ws2818_rgb_led_color: "Color",
-  senseBox_ws2818_rgb_led_number: "Number",
+    "Csatlakoztassa az RGB LED-et a három **digitális/analóg port** egyikéhez. Ha több RGB LED van egymáshoz fűzve, akkor a pozíció alapján meghatározhatja, hogy melyik LED-et vezérli.",
+  senseBox_ws2818_rgb_led_color: "Szín",
+  senseBox_ws2818_rgb_led_number: "Szám",
 
   /**
-   * Color
+   * Szín
    */
 
-  COLOUR_BLEND_COLOUR1: "colour 1",
-  COLOUR_BLEND_COLOUR2: "colour 2",
+  COLOUR_BLEND_COLOUR1: "szín 1",
+  COLOUR_BLEND_COLOUR2: "szín 2",
   COLOUR_BLEND_HELPURL: "http://meyerweb.com/eric/tools/color-blend/",
-  COLOUR_BLEND_RATIO: "ratio",
+  COLOUR_BLEND_RATIO: "arány",
   COLOUR_BLEND_TITLE: "blend",
   COLOUR_BLEND_TOOLTIP:
-    "Blends two colours together with a given ratio (0.0 - 1.0).",
+    "Két szín keverése adott arányban (0.0 - 1.0).",
   COLOUR_PICKER_HELPURL: "https://en.wikipedia.org/wiki/Color",
-  COLOUR_PICKER_TOOLTIP: "Choose a colour from the palette.",
+  COLOUR_PICKER_TOOLTIP: "Válasszon ki egy színt a palettáról.",
   COLOUR_RANDOM_HELPURL: "http://randomcolour.com",
-  COLOUR_RANDOM_TITLE: "random colour",
-  COLOUR_RANDOM_TOOLTIP: "Choose a colour at random.",
-  COLOUR_RGB_BLUE: "blue",
-  COLOUR_RGB_GREEN: "green",
+  COLOUR_RANDOM_TITLE: "véletlen szín",
+  COLOUR_RANDOM_TOOLTIP: "Véletlenszerű szín kiválasztása.",
+  COLOUR_RGB_BLUE: "kék",
+  COLOUR_RGB_GREEN: "zöld",
   COLOUR_RGB_HELPURL: "http://www.december.com/html/spec/colorper.html",
   COLOUR_RGB_RED: "red",
-  COLOUR_RGB_TITLE: "colour with",
+  COLOUR_RGB_TITLE: "színnel",
   COLOUR_RGB_TOOLTIP:
-    "Create a colour with the specified amount of red, green, and blue. All values must be between 0 and 255.",
+    "Hozzon létre egy színt a megadott mennyiségű vörössel, zölddel és kékkel. Minden értéknek 0 és 255 között kell lennie.",
 };
