d42417dd70b1a613c162af20d595755ca7325c4f
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 11:02:06 +0100

    Update sensebox-sensors.js

--- src/components/Blockly/msg/hu/sensebox-sensors.js
+++ src/components/Blockly/msg/hu/sensebox-sensors.js
@@ -1,6 +1,6 @@
 export const SENSORS = {
   /**
-   * Sensors
+   * Érzékelők
    * ---------------------------------------------------
    *
    */
@@ -9,69 +9,69 @@ export const SENSORS = {
    * BMP280
    */
   senseBox_pressure_sensor: "Airpressure/Temperature Sensor (BMP280)",
-  senseBox_pressure: "Airpressure in hPa",
+  senseBox_pressure: "Légnyomás hPa-ban",
   senseBox_pressure_dimension: "Airpressure in hPa",
   senseBox_pressure_tooltip:
-    "Connect the sensor to one of the 5 **I2C ports**. The sensor gives you the measured value for the air pressure in hPa. To calculate the correct altitude above sea level the sensor needs a current reference value.",
-  senseBox_pressure_referencePressure: "Pressure at Sea Level",
+    "Csatlakoztassa az érzékelőt az 5 **I2C port** egyikéhez. Az érzékelő megadja a légnyomás mért értékét hPa-ban. A tengerszint feletti helyes magasság kiszámításához az érzékelőnek szüksége van egy aktuális referenciaértékre.",
+  senseBox_pressure_referencePressure: "Nyomás a tengerszinten",
   senseBox_pressure_referencePressure_dim: "hPa",
   senseBox_pressure_helpurl: "",
 
   /**
    * DPS310
    */
-  senseBox_sensor_dps310: "Airpressure/Temperature Sensor (DPS310)",
+  senseBox_sensor_dps310: "Légnyomás/hőmérséklet érzékelő (DPS310)",
   senseBox_sensor_dps310_tooltip:
-    "Connect the sensor to one of the 5 **I2C ports**. The sensor gives you the measured value for the air pressure in hPa. To calculate the correct altitude above sea level the sensor needs a current reference value.",
+    "Csatlakoztassa az érzékelőt az 5 **I2C port** egyikéhez. Az érzékelő megadja a légnyomás mért értékét hPa-ban. A tengerszint feletti helyes magasság kiszámításához az érzékelőnek szüksége van egy aktuális referenciaértékre.",
 
   /**
    * Mikro
    */
-  senseBox_sound: "Microphone",
+  senseBox_sound: "Mikrofon",
   senseBox_sound_tip:
-    "Connect the sensor to one of the 3 **analog/digital** ports via the breadbord. Returns the reading of the microphone in volt",
+    "Csatlakoztassa az érzékelőt a 3 **analóg/digitális** port egyikéhez a kenyérvágón keresztül. Visszaadja a mikrofon leolvasását voltban",
   senseBox_sound_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-mikro/",
 
   /**
-   * Temperature and Humidity Sensor (HDC1080)
+   * Hőmérséklet- és páratartalom-érzékelő (HDC1080)
    */
-  senseBox_temp: "Temperature in °C",
-  senseBox_temp_hum: "Temperature/Humidity Sensor (HDC1080)",
+  senseBox_temp: "Hőmérséklet °C-ban",
+  senseBox_temp_hum: "Hőmérséklet/páratartalom érzékelő (HDC1080)",
   senseBox_temp_hum_tooltip:
-    "This block returns the temperature and humidity sensor readings. Connect the sensor to one of the 5 I2C ports. Measured value is output with 2 decimal places.",
+    "Ez a blokk a hőmérséklet- és páratartalom-érzékelő leolvasott értékeit adja vissza. Csatlakoztassa az érzékelőt az 5 I2C port egyikéhez. A mért értéket 2 tizedesjegy pontossággal adja ki.",
   senseBox_temp_hum_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-temperatur-luftfeuchte/",
-  senseBox_hum: "humidity in %",
-  senseBox_hum_tip: "Measures humidity in %",
+  senseBox_hum: "páratartalom %-ban",
+  senseBox_hum_tip: "Méri a páratartalmat %-ban",
 
   /**
    * Ultraschalldistanzsensor
    */
 
-  senseBox_ultrasonic: "Ultrasonic distance sensor at Port",
+  senseBox_ultrasonic: "Ultrahangos távolságérzékelő a kikötőben",
   senseBox_ultrasonic_trigger: "Trigger",
   senseBox_ultrasonic_echo: "Echo",
   senseBox_ultrasonic_maxDistance: "Max distance",
   senseBox_ultrasonic_port_A: "A",
   senseBox_ultrasonic_port_B: "B",
   senseBox_ultrasonic_port_C: "C",
-  senseBox_ultrasonic_tooltip: `Measures the distance using ultrasound in cm. Connect the sensor to one of the three digital/analog ports:
-- Trigger: Green Cable
-- Echo: Yellow Cable
-If the max distance is reached the a value of **O** will be returned`,
+  senseBox_ultrasonic_tooltip: `Méri a távolságot ultrahanggal cm-ben. Csatlakoztassa az érzékelőt a három digitális/analóg port egyikéhez:
+- Trigger: Zöld kábel
+- Echo: Sárga kábel
+Ha a maximális távolságot elérte, akkor **O** értéket kap vissza`,
   senseBox_ultrasonic_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-distanz/",
 
   /**
-   * UV and Lightsensor
+   * UV és fényérzékelő
    */
-  senseBox_uv: "UV-Light in µW/cm²",
-  senseBox_uv_light: "Light Visible + UV",
+  senseBox_uv: "UV-fény µW/cm²-ben",
+  senseBox_uv_light: "Látható fény + UV",
   senseBox_value: "Value:",
   senseBox_uv_light_tooltip:
-    "Sensor measures UV light or brightness. Brightness is output as **integer** in lux. UV intensity as **decimal** in µW/cm².",
-  senseBox_light: "Illuminance in Lux",
+    "Az érzékelő az UV-fényt vagy a fényerőt méri. A fényerősség **integrál** luxban kifejezve kerül kimenetre. Az UV-intenzitás **decimális** értékben, µW/cm²-ben.",
+  senseBox_light: "Megvilágítottság Luxban",
   senseBox_uv_light_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-helligkeit-uv/",
 
@@ -89,15 +89,15 @@ If the max distance is reached the a value of **O** will be returned`,
   senseBox_bmx055_accelerometer_direction: "Direction",
   senseBox_bmx055_accelerometer_direction_x: "X-Axis",
   senseBox_bmx055_accelerometer_direction_y: "Y-Axis",
-  senseBox_bmx055_accelerometer_direction_z: "Z-Axis",
+  senseBox_bmx055_accelerometer_direction_z: "Z-tengely",
   senseBox_bmx055_accelerometer_direction_total: "Total",
   senseBox_bmx055_gyroscope: "Gyroscope",
 
   senseBox_bmx055_compass_tip: "Lage Sensor",
   senseBox_bmx055_gyroscope_tip: "Lage Sensor",
-  senseBox_bmx055_x: "X-Direction",
-  senseBox_bmx055_y: "Y-Direction",
-  senseBox_bmx055_accelerometer_tooltip: `This block gives you the measurement value of the accelerometer which is soldered directly on the senseBox MCU. In the dropdown menu you can select the direction and the measuring range.`,
+  senseBox_bmx055_x: "X-irány",
+  senseBox_bmx055_y: "Y-irány",
+  senseBox_bmx055_accelerometer_tooltip: "Ez a blokk a közvetlenül a senseBox MCU-ra forrasztott gyorsulásmérő mérési értékét adja meg. A legördülő menüben kiválaszthatja az irányt és a mérési tartományt.",
   senseBox_bmx055_helpurl: "",
 
   /**
@@ -107,20 +107,21 @@ If the max distance is reached the a value of **O** will be returned`,
   senseBox_gps_getValues: "GPS Modul",
   senseBox_gps_lat: "latitude",
   senseBox_gps_lng: "longitude",
-  senseBox_gps_alt: "altitude in m",
-  senseBox_gps_speed: "speed in km/h",
-  senseBox_gps_date: "date",
+  senseBox_gps_alt: "magasság m-ben",
+  senseBox_gps_speed: "sebesség km/h-ban",
+  senseBox_gps_date: "dátum",
   senseBox_gps_time: "time",
-  senseBox_gps_timeStamp: "Timestamp (RFC 3339)",
-  senseBox_gps_tooltip: `Reads the GPS module and gives you the location information. Longitude and latitude are output as a decimal number with 6 decimal places. 
-**Connection: I2C**
+  senseBox_gps_timeStamp: "(RFC 3339)",
+  senseBox_gps_tooltip: `
+  A GPS-modul beolvasása és a helymeghatározási információk megadása. A hosszúsági és szélességi fokot 6 tizedesjegyű tizedes számként adja ki 
+  **Connection: I2C**
 
-**Note:** The GPS module needs a relatively long time (about 5-10 minutes) until it has found your location!
+  **Figyelem:** A GPS modulnak viszonylag hosszú időre van szüksége (kb. 5-10 perc), amíg megtalálja a helyzetét!
 `,
   senseBox_gps_helpurl: "https://en.docs.sensebox.de/hardware/sensoren-gps/",
 
   /**
-   * Windspeed
+   * Szélsebesség
    */
   senseBox_windspeed: "Windspeedsensor",
   senseBox_windspeed_tooltip: "",
@@ -130,7 +131,7 @@ If the max distance is reached the a value of **O** will be returned`,
    */
   senseBox_soundsensor_dfrobot: "Soundsensor (DF Robot)",
   senseBox_soundsensor_dfrobot_tooltip:
-    "Connect the sensor to one of the 3 **digital/analog ports**. The sensor will give you the reading in dB with one decimal.",
+    "Csatlakoztassa az érzékelőt a 3 **digitális/analóg port** egyikéhez. Az érzékelő dB-ben adja meg a leolvasott értéket egy tizedesjegy pontossággal.",
   senseBox_soundsensor_dfrobot_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-lautstaerke/",
 
@@ -138,67 +139,67 @@ If the max distance is reached the a value of **O** will be returned`,
    * BME680
    */
 
-  senseBox_bme680: "Environmental sensor (BME680)",
-  senseBox_bme_iaq: "Indoor Air Quality (IAQ)",
-  senseBox_bme_iaq_accuracy: "Calibration Value",
-  senseBox_bme_pressure: "Airpressure in Pa",
-  senseBox_bme_co2: "CO2 Equivalent",
-  senseBox_bme_breatheVocEquivalent: "Breathe VOC Equivalent",
-  senseBox_bme_tooltip: `Connect the environmental sensor to one of the 5 **I2C ports**. **Note:** The sensor needs some time to calibrate. 
-The status of the calibration can be read from the calibration value. It is either 0, 1, 2 or 3 and says the following:
+  senseBox_bme680: "Környezeti érzékelő (BME680)",
+  senseBox_bme_iaq: "Beltéri levegőminőség (IAQ)",
+  senseBox_bme_iaq_accuracy: "Kalibrációs érték",
+  senseBox_bme_pressure: "Levegőnyomás Pa-ban",
+  senseBox_bme_co2: "CO2-egyenérték",
+  senseBox_bme_breatheVocEquivalent: "Belégzett VOC-egyenérték",
+  senseBox_bme_tooltip: `Kapcsolja a környezeti érzékelőt az 5 **I2C port** egyikéhez. **Figyelem:** Az érzékelőnek némi időre van szüksége a kalibráláshoz.
+A kalibrálás állapota leolvasható a kalibrációs értékből. Ez vagy 0, 1, 2 vagy 3, és a következőket mondja ki:
 
-- IAQ Accuracy : 0 means sensor is stabilized (takes about 25 minutes) or that there was a timeout,
-- IAQ Accuracy : 1 means value is inaccurate,
-- IAQ Accuracy : 2 means sensor is being calibrated,
-- IAQ Accuracy : 3 means sensor calibrated successfully.
+- IAQ pontosság : 0 azt jelenti, hogy az érzékelő stabilizálódott (kb. 25 percig tart), vagy hogy időkiesés történt,
+- IAQ pontosság : 1 azt jelenti, hogy az érték pontatlan,
+- IAQ pontosság : 2 azt jelenti, hogy az érzékelőt kalibrálják,
+- IAQ pontosság : 3 azt jelenti, hogy az érzékelőt sikeresen kalibrálták.
 
-The measured values for temperature, humidity and air pressure can be used directly,`,
+A hőmérséklet, a páratartalom és a légnyomás mért értékei közvetlenül felhasználhatók,`,
   senseBox_bme680_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-umweltsensor/",
 
   /**
    * Truebner SMT50
    */
-  senseBox_smt50: "Soil Moisture and Temperature (SMT50)",
+  senseBox_smt50: "Talajnedvesség és hőmérséklet (SMT50)",
   senseBox_smt50_helpurl:
     "https://docs.sensebox.de/hardware/sensoren-truebner/",
   senseBox_smt50_tooltip:
     "Schließe den Bodenfeuchtigkeit- und Temperatursensor an einen der 3 digital/analog Ports an und wähle den Port im Block aus. Der Sensor gibt die Bodentemperatur in °C und die Bodenfeuchtigkeit in % aus.",
 
   /**
-   * SCD30 CO2 Sensor
+   * SCD30 CO2-érzékelő
    */
-  senseBox_scd30: "CO2 Sensor (Sensirion SCD30)",
+  senseBox_scd30: "CO2-érzékelő (Sensirion SCD30)",
   senseBox_scd_tooltip:
-    "Connect the sensor to one of the 5 **I2C ports**. The sensor will give you the reading for the CO2 concentration in ppm. The temperature reading may differ from the real value by a few degrees due to the measurement directly at the sensor.",
-  senseBox_scd_co2: "CO2 in ppm",
+    "Csatlakoztassa az érzékelőt az 5 **I2C port** egyikéhez. Az érzékelő a CO2-koncentráció ppm-ben kifejezett értékét fogja megadni. A közvetlenül az érzékelőn végzett mérés miatt a hőmérséklet leolvasása néhány fokkal eltérhet a valós értéktől.",
+  senseBox_scd_co2: "CO2 ppm-ben",
   senseBox_scd_helpurl: "https://en.docs.sensebox.de/hardware/sensoren-co2/",
   /**
-   * Feinstaubsensor (SDS011)
+   * Feinstaubsenzor (SDS011)
    */
   senseBox_sds011: "Fine Particular Sensor",
   senseBox_sds011_dimension: "in µg/m³ at",
   senseBox_sds011_pm25: "PM2.5",
   senseBox_sds011_pm10: "PM10",
   senseBox_sds011_tooltip:
-    "This block gives you the reading of the fine dust sensor. Connect the fine dust sensor to one of the 2 **Serial/UART** ports. Select between PM2.5 and PM10 in the dropdown menu. The measured value will be displayed as **comma number** in µg/m3",
+    "Ez a blokk a finompor-érzékelő leolvasását adja meg. Csatlakoztassa a finom porérzékelőt a 2 **Serial/UART** port egyikéhez. Válasszon a PM2.5 és a PM10 között a legördülő menüben. A mért érték **komma szám** formájában jelenik meg µg/m3-ban",
   senseBox_sds011_serial1: "Serial1",
   senseBox_sds011_serial2: "Serial2",
   senseBox_sds011_helpurl:
     "https://en.docs.sensebox.de/hardware/sensoren-feinstaub/",
 
   /**
-   * Button
+   * Gomb
    */
   senseBox_button: "Button",
   senseBox_button_isPressed: "is Pressed",
   senseBox_button_switch: "as Switch",
   senseBox_button_wasPressed: "was Pressed",
   senseBox_button_longPress: "Pressed for",
-  senseBox_button_tooltip: `This block gives you the status of the connected button. In the dropdown menu you can select different modes for the button. Either the on board button or a button connected to one of the 6 digital pins can be controlled. different modes:
-- "is pressed": With this mode you can check if the block is currently pressed. You get either the value TRUE or FALSE.
-- "was pressed": With this mode you can query if the block was pressed. Only if the button was pressed and released you will get TRUE.
-- "as switch": If you use this block you can use the button like a light switch. The status is saved until the button is pressed again.
+  senseBox_button_tooltip: `Ez a blokk a csatlakoztatott gomb állapotát mutatja. A legördülő menüben különböző módokat választhat a gombhoz. Vagy a fedélzeti gombot, vagy a 6 digitális pin valamelyikére csatlakoztatott gombot lehet vezérelni. különböző módokat:
+- "Meg van nyomva": Ezzel az üzemmóddal ellenőrizheti, hogy a blokk jelenleg nyomva van-e. Vagy a TRUE vagy a FALSE értéket kapja.
+- "volt megnyomva": Ezzel az üzemmóddal lekérdezheti, hogy a blokkot megnyomták-e. Csak akkor kapod a TRUE értéket, ha a gombot megnyomták és elengedték.
+- "mint kapcsoló": Ha ezt a blokkot használod, akkor a gombot úgy használhatod, mint egy villanykapcsolót. Az állapot addig marad meg, amíg a gombot újra meg nem nyomja.
 `,
   senseBox_button_helpurl: "",
 
@@ -212,14 +213,14 @@ The measured values for temperature, humidity and air pressure can be used direc
    * Feinstaubsensor Sensirion SPS30
    */
 
-     senseBox_sps30: "Particulate Matter Sensor (Sensirion SPS30)",
+     senseBox_sps30: "Porszemcsés anyag érzékelő (Sensirion SPS30)",
      senseBox_sps30_dimension: "in µg/m³",
      senseBox_sps30_1p0: "PM1.0",
      senseBox_sps30_2p5: "PM2.5",
      senseBox_sps30_4p0: "PM4.0",
      senseBox_sps30_10p0: "PM10",
      senseBox_sps30_tooltip:
-     "This block gives you the measured value of the Sensirion SPS30 fine dust sensor. Connect the fine dust sensor to one of the 5 **I2C** connectors. Select between PM1.0, PM2.5, PM4.0 and PM10 in the dropdown menu. The measured value will be displayed as **decimal** in µg/m3",
+     "Ez a blokk a Sensirion SPS30 finom porszenzor mért értékét adja meg. Csatlakoztassa a finompor-érzékelőt az 5 **I2C** csatlakozó egyikéhez. Válasszon a PM1.0, PM2.5, PM4.0 és PM10 között a legördülő menüben. A mért érték **decimális** értékként jelenik meg µg/m3-ban",
      senseBox_sps30_helpurl:
        "https://docs.sensebox.de/hardware/sensoren-feinstaub/",
    
