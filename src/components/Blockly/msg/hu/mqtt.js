58cf976a3df34ed48ceba7b20356cafb560963c6
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 10:54:05 +0100

    Update mqtt.js

--- src/components/Blockly/msg/hu/mqtt.js
+++ src/components/Blockly/msg/hu/mqtt.js
@@ -4,13 +4,13 @@ export const MQTT = {
      * MQTT
      */
 
-    senseBox_mqtt_init: "Connect to MQTT Broker",
+    senseBox_mqtt_init: "Csatlakozás az MQTT brókerhez",
     senseBox_mqtt_server: "Server",
     senseBox_mqtt_port: "Port",
-    senseBox_mqtt_username: "Username",
-    senseBox_mqtt_password: "Password",
+    senseBox_mqtt_username: "Felhasználónév",
+    senseBox_mqtt_password: "Jelszó",
     sensebox_mqtt_subscribe: "Subscribe to Feed",
     senseBox_mqtt_publish: "Publish to Feed/Topic",
-    senseBox_mqtt_init_tooltip: "Initialise a connection to a MQTT Broker. Use this Block inside the setup()-function and make sure to connect to your wifi first.",
+    senseBox_mqtt_init_tooltip: "A kapcsolat inicializálása egy MQTT brókerrel. Használja ezt a blokkot a setup()-függvényen belül, és győződjön meg róla, hogy először csatlakozik a wifi-hez.",
     senseBox_mqtt_publish_tooltip: "Publish something to a specific topic to your MQTT Broker.",
 }
