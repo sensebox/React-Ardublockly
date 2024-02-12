89d8ac2913f821903b9e106b96a7ebe9102b5bac
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-12 10:54:18 +0100

    Update sensebox-ble.js

--- src/components/Blockly/msg/hu/sensebox-ble.js
+++ src/components/Blockly/msg/hu/sensebox-ble.js
@@ -3,23 +3,23 @@ export const BLE = {
    * Phyphox Init
    */
   sensebox_phyphox_init: "Initialise Phyphox device with name:",
-  sensebox_phyphox_createExperiment: "Create experiment",
-  sensebox_phyphox_experimentName: "Name of experiment",
+  sensebox_phyphox_createExperiment: "Create experiment": "Kísérlet létrehozása",
+  sensebox_phyphox_experimentName: "A kísérlet neve",
   sensebox_phyphox_experimentTitle: "Title",
   sensebox_phyphox_experimentCategory: "Category",
-  sensebox_phyphox_experimentDescription: "Description",
+  sensebox_phyphox_experimentDescription: "Description": "Leírás",
   sensebox_phyphox_experiment_description:
-    "Short description of the experiment",
-  sensebox_phyphox_writeValues: "Send values",
-  sensebox_phyphox_createView: "With graphs:",
-  sensebox_phyphox_createGraph: "Create Graph",
+    "A kísérlet rövid leírása",
+  sensebox_phyphox_writeValues: "Értékeket küldeni",
+  sensebox_phyphox_createView: "A grafikonokkal:",
+  sensebox_phyphox_createGraph: "Create Graph": "Create Graph",
   sensebox_phyphox_graphLabel: "",
   sensebox_phyphox_unitx: "Unit x-axis",
   sensebox_phyphox_unity: "Unit y-axis",
-  sensebox_phyphox_labelx: "Label x-axis",
+  sensebox_phyphox_labelx: "Címke x-tengely",
   sensebox_phyphox_labely: "Label y-axis",
-  sensebox_phyphox_channel0: "x-axis value",
-  sensebox_phyphox_channel1: "y-axis value",
+  sensebox_phyphox_channel0: "x-tengely értéke",
+  sensebox_phyphox_channel1: "y-tengely értéke",
   sensebox_phyphox_style_dots: "Dots",
   sensebox_phyphox_style_line: "Line",
   sensebox_phyphox_timestamp: "Timestamp",
@@ -28,17 +28,17 @@ export const BLE = {
   sensebox_phyphox_graphStyle: "style",
 
   sensebox_phyphox_init_tooltip:
-    "Initialise the Bluetooth Bee. Plug it into the **XBEE1** slot. Give the Phyphox meter a unique name so you can find it in the app",
+    "Initialise the Bluetooth Bee. Csatlakoztassa a **XBEE1** foglalathoz. Adjon a Phyphox mérőnek egy egyedi nevet, hogy az alkalmazásban megtalálhassa",
   sensebox_phyphox_helpurl:
     "https://sensebox.de/projects/de/2021-12-21-verbindung%20mit%20der%20phyphox%20app",
   sensebox_phyphox_experiment_tooltip:
-    "Create an experiment and give it a unique name and a short description. Add up to 5 different graphs in the view. ",
+    "Create an experiment and give it a one unique name and a short description. Adjon hozzá legfeljebb 5 különböző grafikont a nézetben. ",
   sensebox_phyphox_graph_tooltip:
-    "Creates a new graph for the experiment. Specify the unit and label for the axes and choose the style of visualisation of the measured values. Add to the interfaces for the values of the X- and Y-axis the channel on which the measured values will be sent later. If you want to create a timestamp via the Phyphox app, connect the block *Timestamp*",
+    "Új gráfot hoz létre a kísérlethez. Adja meg a tengelyek egységét és címkéjét, valamint válassza ki a mért értékek megjelenítési stílusát. Adja hozzá az X- és Y-tengely értékeinek interfészéhez azt a csatornát, amelyen a mért értékek később elküldésre kerülnek. Ha időbélyeget szeretne létrehozni a Phyphox alkalmazáson keresztül, csatlakoztassa a *Timestamp*" blokkot,
   sensebox_phyphox_timestamp_tooltip:
-    "Use this block to have a timestamp created via the Phyphox app",
+    "Használja ezt a blokkot, ha időbélyeget szeretne létrehozni a Phyphox alkalmazáson keresztül",
   sensebox_phyphox_sendchannel_tooltip:
-    "Sends a reading to the selected channel",
+    "Olvasást küld a kiválasztott csatornára",
   sensebox_phyphox_experiment_send_tooltip:
-    "Sends the measured values to the Phyphox App",
+    "Elküldi a mért értékeket a Phyphox alkalmazásba",
 };
