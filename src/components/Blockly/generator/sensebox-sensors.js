7826b989ed59ef109e8a9312e3d0981831604e2f
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-14 10:30:03 +0100

    fix pm sensor error
    closes #269

--- src/components/Blockly/generator/sensebox-sensors.js
+++ src/components/Blockly/generator/sensebox-sensors.js
@@ -159,13 +159,26 @@ Blockly.Arduino.sensebox_sensor_sds011 = function () {
   ] = `#include <SdsDustSensor.h> // http://librarymanager/All#Nova_Fitness_Sds_dust_sensors_library`;
   Blockly.Arduino.definitions_["define_sds011"] =
     "SdsDustSensor sds(" + serial_name + ");";
+  Blockly.Arduino.functionNames_["sds011_getPmData()"] = `
+float getPmData(int type) {
+  PmResult pm = sds.queryPm();
+  if (pm.isOk()) {
+    if (type == 25){
+      return pm.pm25;
+    } else if (type == 10) {
+      return pm.pm10;
+    }
+  else return 0;
+  }
+}
+`
   Blockly.Arduino.setupCode_["sds011_begin"] = "sds.begin();";
   Blockly.Arduino.setupCode_["sds011_setQueryReportingMode"] =
     "sds.setQueryReportingMode();";
-  Blockly.Arduino.loopCodeOnce_[
-    "sds011_getData"
-  ] = `PmResult pm = sds.queryPm();`;
-  var code = `pm.${dropdown_name}`;
+  // Blockly.Arduino.loopCodeOnce_[
+  //   "sds011_getData"
+  // ] = `PmResult pm = sds.queryPm();`;
+  var code = `getPmData(${dropdown_name})`;
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
