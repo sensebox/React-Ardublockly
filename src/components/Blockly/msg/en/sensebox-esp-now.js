d42276dc92fe77a97f445dc6c86076c78a3487c6
PaulaScharf <paulascharf@gmx.de>
2024-03-20 16:47:56 +0100

    move esp-now translations to seperate file

---
+++ src/components/Blockly/msg/en/sensebox-esp-now.js
@@ -0,0 +1,12 @@
+export const ESP_NOW = {
+  senseBox_esp_now_tooltip: "Initialise ESP-Now",
+  senseBox_esp_now_init_tooltip: "Initialise ESP-Now",
+  senseBox_esp_now_sender: "Connect to receiving senseBox",
+  senseBox_esp_now_sender_tooltip: "Connect to receiving senseBox",
+  senseBox_wifi_mac: "Mac-Address",
+  senseBox_wifi_mac_tooltip: "The Mac-Address of the senseBox",
+  senseBox_esp_now_receive: "When message %1 received",
+  senseBox_esp_now_receive_tooltip: "Do the following, when you receive a message",
+  senseBox_esp_now_send: "Send message to receiver",
+  senseBox_esp_now_send_tooltip: "Send message to receiver",
+  };
