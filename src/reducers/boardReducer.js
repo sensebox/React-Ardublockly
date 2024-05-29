440f1f58c309a7392f953f7eef811ca7466f4b25
Thiemann96 <e_thie10@uni-muenster.de>
2024-05-29 13:34:11 +0200

    fixed background image and compile message

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,6 +1,7 @@
-import { BOARD } from '../actions/types';
-import mini_opacity from "../data/mini_opacity.png"
-import mcu_opacity from "../data/mcu_opacity.png"
+import { BOARD } from "../actions/types";
+import mini_opacity from "../data/mini_opacity.png";
+import mcu_opacity from "../data/mcu_opacity.png";
+import esp_opacity from "../data/esp_opacity.png";
 
 const initialValue = () => {
   if (window.sessionStorage.getItem("board")) {
@@ -11,16 +12,28 @@ const initialValue = () => {
 };
 
 const setBackgroundImage = (board) => {
-  const root = document.querySelector(':root');
-  root.style.setProperty('--url', `url(${board === "mcu" ? mcu_opacity : mini_opacity})`);
-}
-
-const initialState = {
-    board: initialValue()
+  const root = document.querySelector(":root");
+  switch (board) {
+    case "mcu":
+      root.style.setProperty("--url", `url(${mcu_opacity})`);
+      break;
+    case "mini":
+      root.style.setProperty("--url", `url(${mini_opacity})`);
+      break;
+    case "esp32":
+      root.style.setProperty("--url", `url(${esp_opacity})`);
+      break;
+    default:
+      break;
+  }
 };
 
-export default function foo(state = initialState, action){
-  switch(action.type){
+const initialState = {
+  board: initialValue(),
+};
+
+export default function foo(state = initialState, action) {
+  switch (action.type) {
     case BOARD:
       return {
         ...state,
