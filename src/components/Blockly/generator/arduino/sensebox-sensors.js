2f523c012c4ac1675667578aab4f511976b8d5ed
PaulaScharf <paulascharf@gmx.de>
2024-11-25 14:24:59 +0100

    dont give negative distances

--- src/components/Blockly/generator/arduino/sensebox-sensors.js
+++ src/components/Blockly/generator/arduino/sensebox-sensors.js
@@ -415,7 +415,7 @@ Blockly.Generator.Arduino.forBlock["sensebox_tof_imager"] = function () {
             }
           }
         }
-        oldVl53l8cxMin = (min==10000.0) ? 0.0 : min;
+        oldVl53l8cxMin = (min==10000.0 || min < 0.0) ? 0.0 : min;
       }
       return oldVl53l8cxMin;
       }`;
