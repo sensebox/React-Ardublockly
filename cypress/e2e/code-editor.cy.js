a05ff8303ef11a1c3d04a4e3437907fe1bb31267
Felix Erdmann <hello@felixerdmann.com>
2025-02-18 16:05:42 +0100

    test: code editor tests

---
+++ cypress/e2e/code-editor.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="Cypress" />
+
+describe("Code Editor Page Tests", () => {
+  it("[CodeEditor] visits the code editor page", () => {
+    cy.visit("/codeeditor");
+  });
+
+  it("[CodeEditor] selects senseBox ESP", () => {
+    cy.visit("/codeeditor");
+    cy.get('img[alt="Sensebox ESP"]').click();
+  });
+
+  it("[CodeEditor] compiles code", () => {
+    cy.visit("/codeeditor");
+    cy.get('img[alt="Sensebox ESP"]').click();
+    cy.get("button").contains("Kompilieren").click();
+  });
+
+  it("[CodeEditor] opens reset dialog", () => {
+    cy.visit("/codeeditor");
+    cy.get('img[alt="Sensebox ESP"]').click();
+    cy.get("button").contains("Reset code").click();
+    cy.get("button").contains("Zurücksetzen").should("exist");
+  });
+
+  it("[CodeEditor] fetches libraries", () => {
+    // intercept the request to the libraries endpoint
+    cy.intercept({
+      method: "GET",
+      pathname: "/libraries",
+      query: {
+        format: "json",
+      },
+    }).as("getLibraries");
+
+    cy.visit("/codeeditor");
+    cy.get('img[alt="Sensebox ESP"]').click();
+
+    // check if the request was successful
+    cy.wait("@getLibraries").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body).to.have.property("installed_libraries");
+      expect(interception.response.body.installed_libraries).to.be.an("array").that.is.not.empty;
+    });
+  });
+});
