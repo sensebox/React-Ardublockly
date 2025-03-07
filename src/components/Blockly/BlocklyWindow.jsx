04e4f2814c618dc436c638a9b7a44abb19552c86
Eric Thieme-Garmann <erictg96@gmail.com>
2025-03-07 12:56:14 +0100

    disable orphans

--- src/components/Blockly/BlocklyWindow.jsx
+++ src/components/Blockly/BlocklyWindow.jsx
@@ -24,6 +24,7 @@ class BlocklyWindow extends Component {
     const workspace = Blockly.getMainWorkspace();
     this.props.onChangeWorkspace({});
     this.props.clearStats();
+    workspace.addChangeListener(Blockly.Events.disableOrphans);
     workspace.addChangeListener((event) => {
       this.props.onChangeWorkspace(event);
 
