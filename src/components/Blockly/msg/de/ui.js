2f652d7546ad990385f4f854a48215bff0362255
Eric Thieme-Garmann <erictg96@gmail.com>
2025-04-03 14:11:01 +0200

    typo and padding to search

--- src/components/Blockly/msg/de/ui.js
+++ src/components/Blockly/msg/de/ui.js
@@ -341,7 +341,7 @@ export const UI = {
   suggestion_pre_text: "Versuch es mal mit: ",
 
   display_not_declared:
-    "Stelle sicher, dasss du das Display im Setup initialisiert hast.",
+    "Stelle sicher, dass du das Display im Setup initialisiert hast.",
   variable_redeclared:
     "Stelle sicher, dass du keine Sonderzeichen in deinen Variablennamen verwendest. Dazu gehören z.B. Leerzeichen, Sternchen oder Anführungszeichen.",
 
