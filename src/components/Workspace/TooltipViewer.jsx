f91b7eb5c2dca860b1f8b42e413cefbae07c1b7f
Eric Thieme-Garmann <erictg96@gmail.com>
2025-07-10 14:49:50 +0200

    modernized 4 components

--- src/components/Workspace/TooltipViewer.jsx
+++ src/components/Workspace/TooltipViewer.jsx
@@ -1,118 +1,84 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Button, Card } from "@mui/material";
-import * as Blockly from "blockly";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Box,
+  useTheme,
+} from "@mui/material";
 import ReactMarkdown from "react-markdown";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import Slide from "@mui/material/Slide";
-import SensorInfo from "../SensorInfo";
-import store from "../../store";
+import * as Blockly from "blockly";
 
-// FIXME checkout https://mui.com/components/use-media-query/#migrating-from-withwidth
-const withWidth = () => (WrappedComponent) => (props) => (
-  <WrappedComponent {...props} width="xs" />
-);
+const TooltipViewer = () => {
+  const theme = useTheme();
+  const tooltip = useSelector((s) => s.workspace.code.tooltip);
+  const helpurl = useSelector((s) => s.workspace.code.helpurl);
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
-class TooltipViewer extends Component {
-  constructor(props) {
-    super(props);
-    this.inputRef = React.createRef();
-    this.state = {
-      open: false,
-    };
-  }
-
-  toggleDialog = () => {
-    this.setState({ open: !this.state });
-  };
-
-  openDialog = () => {
-    this.setState({ open: true });
-  };
-
-  render() {
-    return (
-      <Card
-        className="helpSection"
-        style={{
-          height: "25vh",
-          overflowY: "scroll",
-          marginTop: "1vh",
-          padding: "1.1rem",
-          borderRadius: "4px",
-          border: "1px solid #ddd",
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <CardContent>
-          <Typography
-            variant="h6"
-            component="h2"
+  return (
+    <Card
+      className="helpSection"
+      sx={{
+        height: "25vh",
+        overflowY: "auto",
+        mt: 2,
+        p: 2,
+        borderRadius: 1,
+        border: `1px solid ${theme.palette.divider}`,
+        boxShadow: 1,
+      }}
+    >
+      <CardContent>
+        <Typography
+          variant="h6"
+          component="h2"
+          style={{
+            marginBottom: "0.5rem",
+            position: "relative",
+            paddingBottom: "0.3rem",
+          }}
+        >
+          <span style={{ display: "inline-block" }}>
+            {Blockly.Msg.tooltip_moreInformation_02}
+          </span>
+          <span
             style={{
-              marginBottom: "0.5rem",
-              position: "relative",
-              paddingBottom: "0.3rem",
+              content: "''",
+              display: "block",
+              width: "50%",
+              height: "4px",
+              backgroundColor: "#4caf50",
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              borderRadius: "2px",
+            }}
+          ></span>
+        </Typography>
+
+        <ReactMarkdown linkTarget="_blank">{tooltip}</ReactMarkdown>
+
+        {helpurl && (
+          <Button
+            variant="contained"
+            color="primary"
+            href={helpurl}
+            target="_blank"
+            sx={{
+              mt: 2,
+              px: 2,
+              py: 1,
+              borderRadius: 1,
+              fontSize: "0.9rem",
             }}
           >
-            <span style={{ display: "inline-block" }}>
-              {Blockly.Msg.tooltip_moreInformation_02}
-            </span>
-            <span
-              style={{
-                content: "''",
-                display: "block",
-                width: "50%",
-                height: "4px",
-                backgroundColor: "#4caf50",
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                borderRadius: "2px",
-              }}
-            ></span>
-          </Typography>
-          <ReactMarkdown linkTarget="_blank">
-            {this.props.tooltip}
-          </ReactMarkdown>
-          {this.props.helpurl && (
-            <Button
-              variant="contained"
-              color="primary"
-              href={this.props.helpurl}
-              target="_blank"
-              style={{
-                padding: "0.5rem 1rem",
-                borderRadius: "5px",
-                fontSize: "0.9rem",
-              }}
-            >
-              Zur Dokumentation
-            </Button>
-          )}
-        </CardContent>
-      </Card>
-    );
-  }
-}
-
-TooltipViewer.propTypes = {
-  tooltip: PropTypes.string,
-  helpurl: PropTypes.string,
+            Zur Dokumentation
+          </Button>
+        )}
+      </CardContent>
+    </Card>
+  );
 };
 
-const mapStateToProps = (state) => ({
-  tooltip: state.workspace.code.tooltip,
-  helpurl: state.workspace.code.helpurl,
-  language: state.general.language,
-});
-
-export default connect(mapStateToProps, null)(withWidth()(TooltipViewer));
+export default TooltipViewer;
