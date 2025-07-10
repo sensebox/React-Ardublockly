9553e3802d51e9ba58a5096c83d461bccab2a845
Eric Thieme-Garmann <erictg96@gmail.com>
2025-07-10 17:41:34 +0200

    add fluoro blocks

---
+++ src/components/Blockly/msg/en/fluoroASM.js
@@ -0,0 +1,5 @@
+export const FLUOROASM = {
+  senseBox_fluoroASM_init: "Initialize FluoroASM",
+  senseBox_fluoroASM_setLED: "Turn on Fluoro LED",
+  senseBox_fluoroASM_setLED2: "Turn on Fluoro LED",
+};
