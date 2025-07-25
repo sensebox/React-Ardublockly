c72ef57632bdd7def9b11e3dd53ff8a4eff1da96
Eric Thieme-Garmann <erictg96@gmail.com>
2025-07-25 12:49:04 +0200

    remove duplicate block, enable turnign off led's

--- src/components/Blockly/blocks/fluoroASM.js
+++ src/components/Blockly/blocks/fluoroASM.js
@@ -25,34 +25,6 @@ Danke!`;
 
 Blockly.Blocks["sensebox_fluoroASM_setLED"] = {
   init: function () {
-    this.appendDummyInput()
-      .appendField(Blockly.Msg.senseBox_fluoroASM_setLED)
-      .appendField(
-        new Blockly.FieldDropdown([
-          ["1", "1"],
-          ["2", "2"],
-          ["3", "3"],
-          ["4", "4"],
-        ]),
-        "LED_NUMBER",
-      );
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(getColour().sensebox);
-    const text = `Mit dem QOOOL Fluoro kannst Du die Grundlagen von Fluoreszenz erforschen – direkt in der Blockly-Umgebung. In diesem digitalen Experiment steuerst du farbige LEDs und kombinierst sie mit verschiedenen Farbfiltern. Besonders spannend: manche Filter enthalten einen Mikrodiamanten mit sogenannten NV-Zentren (Stickstoff-Leerstellen-Zentren). Diese speziellen Defekte können durch Licht angeregt werden und geben dann einen Teil der Energie in Form von sichtbarem Licht wieder ab. Dieser Effekt heißt Fluoreszenz.
-Was kannst Du im Simulator ausprobieren? 
-- Schalte verschiedene LEDs (rot, grün, blau, weiß) ein oder aus \n
-- Lege verschiedene Farbfilter über die Lichtquelle und beobachte, wie sie das Licht verändern \n
-- Füge einen Mikrodiamanten in den Filter ein oder entferne ihn, und achte darauf, ob Fluoreszenz sichtbar wird \n
-- Vergleiche verschiedene Kombinationen – welche führen zu leuchtenden Effekten? \n
-Viel Spaß beim Entdecken der Fluoreszenz! 
-Danke!`;
-    this.setTooltip(text);
-  },
-};
-
-Blockly.Blocks["sensebox_fluoroASM_setLED2"] = {
-  init: function () {
     this.setColour(getColour().sensebox);
     this.appendDummyInput()
       .appendField(Blockly.Msg.fluoro_led)
