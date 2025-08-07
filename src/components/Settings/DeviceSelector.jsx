9e7f9116fb00cf92d47e7cb26e8ccf55242f5b9b
Paula Scharf <paulascharf@gmx.de>
2025-08-07 18:23:48 +0200

    sensebox eye baseline

--- src/components/Settings/DeviceSelector.jsx
+++ src/components/Settings/DeviceSelector.jsx
@@ -35,11 +35,29 @@ export default function DeviceSelector() {
     const value = e.target.value;
     dispatch(setBoard(value));
 
+    let boardName;
+    switch (value) {
+      case "mcu":
+        boardName = "senseBox MCU";
+        break;
+      case "mini":
+        boardName = "senseBox MCU mini";
+        break;
+      case "esp32":
+        boardName = "senseBox MCU-S2";
+        break;
+      case "eye":
+        boardName = "senseBox Eye";
+        break;
+      default:
+        boardName = value;
+    }
+
     setSnackInfo({
       type: "success",
       key: Date.now(),
       // you can replace this with a Blockly.Msg key if available
-      message: `${Blockly.Msg.settings_board} geändert: ${value === "MCU" ? "senseBox MCU" : value === "MCU:MINI" ? "senseBox MCU mini" : "senseBox MCU-S2"}`,
+      message: `${Blockly.Msg.settings_board} geändert: ${boardName}`,
     });
     setSnackbarOpen(true);
   };
@@ -69,9 +87,10 @@ export default function DeviceSelector() {
           value={selectedBoard}
           onChange={handleChange}
         >
-          <MenuItem value="MCU">senseBox MCU</MenuItem>
-          <MenuItem value="MCU:mini">senseBox MCU mini</MenuItem>
-          <MenuItem value="MCU-S2">senseBox MCU-S2</MenuItem>
+          <MenuItem value="mcu">senseBox MCU</MenuItem>
+          <MenuItem value="mini">senseBox MCU mini</MenuItem>
+          <MenuItem value="esp32">senseBox MCU-S2</MenuItem>
+          <MenuItem value="eye">senseBox Eye</MenuItem>
         </Select>
       </FormControl>
 
