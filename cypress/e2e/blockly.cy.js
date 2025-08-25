825519a9825cb7c37610cac4aaa2aa88fd941d33
Eric Thieme-Garmann <erictg96@gmail.com>
2025-08-25 10:20:27 +0200

    add: tests for mcu

--- cypress/e2e/blockly.cy.js
+++ cypress/e2e/blockly.cy.js
@@ -89,7 +89,7 @@ describe("Blockly Editor Page Tests", () => {
       .and("contain.text", "Sprache");
   });
 
-  it("[Blockly] changes to tablet mode and compiles code", () => {
+  it("[Blockly] changes to tablet mode and compiles code for esp32", () => {
     cy.intercept({
       method: "POST",
       pathname: "/compile",
@@ -102,6 +102,43 @@ describe("Blockly Editor Page Tests", () => {
     cy.contains("button", "Close").click();
     cy.get('img[alt="Sensebox ESP"]').click();
     cy.get('button[aria-label="Compile code"]').click();
+
+    // check if the request was made
+    cy.wait("@compile", {
+      responseTimeout: 30000,
+      requestTimeout: 30000,
+    }).then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body).to.have.property("data");
+      expect(interception.response.body.data).to.have.property("id");
+      expect(interception.response.body.data.id).to.be.a("string");
+    });
+  });
+
+  it("[Blockly] changes to tablet mode and compiles code for MCU", () => {
+    cy.intercept({
+      method: "POST",
+      pathname: "/compile",
+    }).as("compile");
+
+    cy.visit("/settings");
+    cy.get("#ota-selector").click();
+    cy.contains("li", "Activated").click();
+    cy.visit("/");
+    cy.contains("button", "Close").click();
+    cy.get('img[alt="Sensebox MCU"]').click();
+    cy.get('button[aria-label="Compile code"]').click();
+
+    // check if the request was made
+    cy.wait("@compile", {
+      responseTimeout: 30000,
+      requestTimeout: 30000,
+    }).then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body).to.have.property("data");
+      expect(interception.response.body.data).to.have.property("id");
+      expect(interception.response.body.data.id).to.be.a("string");
+    });
   });
 
   it("[Blockly] compiles code", () => {
