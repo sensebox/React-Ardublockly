4739f46a1d4921312097c55e6ad86128d96e2bb0
Eric Thieme-Garmann <erictg96@gmail.com>
2025-09-02 12:50:11 +0200

    remove: bmp280 sensor

--- src/components/Blockly/toolbox/ToolboxEsp.jsx
+++ src/components/Blockly/toolbox/ToolboxEsp.jsx
@@ -20,7 +20,6 @@ export const ToolboxEsp = () => {
         <Block type="sensebox_esp32s2_accelerometer" />
         <Block type="sensebox_sensor_sds011" />
         <Block type="sensebox_sensor_sps30" />
-        <Block type="sensebox_sensor_pressure" />
         <Block type="sensebox_sensor_dps310" />
         <Block type="sensebox_sensor_bme680_bsec" />
         <Block type="sensebox_scd30" />
