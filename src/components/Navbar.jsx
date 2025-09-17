3c6a2acde24db3ae28ddd3aa01f2fa72def55a61
Eric Thieme-Garmann <erictg96@gmail.com>
2025-09-17 16:03:34 +0200

    add: search and difficulty filter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -519,17 +519,6 @@ const Navbar = () => {
           )}
         </List>
       </Drawer>
-
-      {(tutorialIsLoading || projectIsLoading) && (
-        <LinearProgress
-          style={{
-            marginBottom: "30px",
-            boxShadow:
-              "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
-          }}
-        />
-      )}
-
       <Tour
         steps={isHome ? home() : assessment()}
         isOpen={isTourOpen}
