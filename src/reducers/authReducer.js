913825c6433a5a34bec3cb32a5afc79d95745b7d
Eric Thieme-Garmann <erictg96@gmail.com>
2025-10-20 19:17:12 +0200

    add: different login

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -36,6 +36,7 @@ export default function foo(state = initialState, action) {
       };
     case LOGIN_SUCCESS:
     case REFRESH_TOKEN_SUCCESS:
+      console.log("Hallo");
       localStorage.setItem("token", action.payload.token);
       localStorage.setItem("refreshToken", action.payload.refreshToken);
       return {
