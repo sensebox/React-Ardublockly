4903ac32b4f00fbe6e8fff410e5796a8e4bf32dc
Maria Zadnepryanets <mariya.z@gmail.com>
2026-01-27 14:14:45 +0100

    feat: don't show social icons when sharing project on /embedded path

--- src/components/Workspace/ToolbarItems/ShareProject.jsx
+++ src/components/Workspace/ToolbarItems/ShareProject.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { shareProject } from "../../../actions/projectActions";
 import { clearMessages } from "../../../actions/messageActions";
 import QRCode from "qrcode.react";
@@ -315,45 +315,47 @@ class ShareProject extends Component {
                   Download QR code
                 </Button>
               </div>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "20px",
-                }}
-              >
-                <FacebookShareButton
-                  url={this.state.shortLink}
-                  quote={"I created this sketch for my senseBox. Have a look!"}
-                  hashtag={"#senseBox"}
+              {!this.props.isEmbedded && (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "20px",
+                  }}
                 >
-                  <FacebookIcon size={32} round />
-                </FacebookShareButton>
-                <TwitterShareButton
-                  url={this.state.shortLink}
-                  title={"I created this sketch for my senseBox. Have a look!"}
-                  hashtags={["senseBox", "Blockly", "citizenScience"]}
-                >
-                  <TwitterIcon size={32} round />
-                </TwitterShareButton>
-                <WhatsappShareButton
-                  url={this.state.shortLink}
-                  title={
-                    "Look at my SenseBox sketch that I created with Blockly!"
-                  }
-                  separator={": "}
-                >
-                  <WhatsappIcon size={32} round />
-                </WhatsappShareButton>
-                <EmailShareButton
-                  url={this.state.shortLink}
-                  subject={"SenseBox Blockly Sketch"}
-                  body={"I created this sketch for my senseBox. Have a look!"}
-                  separator={": "}
-                >
-                  <EmailIcon size={32} round />
-                </EmailShareButton>
-              </div>
+                  <FacebookShareButton
+                    url={this.state.shortLink}
+                    quote={"I created this sketch for my senseBox. Have a look!"}
+                    hashtag={"#senseBox"}
+                  >
+                    <FacebookIcon size={32} round />
+                  </FacebookShareButton>
+                  <TwitterShareButton
+                    url={this.state.shortLink}
+                    title={"I created this sketch for my senseBox. Have a look!"}
+                    hashtags={["senseBox", "Blockly", "citizenScience"]}
+                  >
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
+                  <WhatsappShareButton
+                    url={this.state.shortLink}
+                    title={
+                      "Look at my SenseBox sketch that I created with Blockly!"
+                    }
+                    separator={": "}
+                  >
+                    <WhatsappIcon size={32} round />
+                  </WhatsappShareButton>
+                  <EmailShareButton
+                    url={this.state.shortLink}
+                    subject={"SenseBox Blockly Sketch"}
+                    body={"I created this sketch for my senseBox. Have a look!"}
+                    separator={": "}
+                  >
+                    <EmailIcon size={32} round />
+                  </EmailShareButton>
+                </div>
+              )}
               {this.props.project &&
               this.props.project.shared &&
               this.props.message.id !== "SHARE_SUCCESS" ? (
@@ -405,9 +407,16 @@ ShareProject.propTypes = {
 const mapStateToProps = (state) => ({
   name: state.workspace.name,
   message: state.message,
-  isEmbedded: state.general.embeddedMode,
 });
 
-export default connect(mapStateToProps, { shareProject, clearMessages })(
+const ShareProjectWithStyles = connect(mapStateToProps, { shareProject, clearMessages })(
   withStyles(styles, { withTheme: true })(ShareProject),
 );
+
+// Wrapper component to get isEmbedded from hook
+const ShareProjectWrapper = (props) => {
+  const isEmbedded = useSelector((state) => state.general.embeddedMode);
+  return <ShareProjectWithStyles {...props} isEmbedded={isEmbedded} />;
+};
+
+export default ShareProjectWrapper;
