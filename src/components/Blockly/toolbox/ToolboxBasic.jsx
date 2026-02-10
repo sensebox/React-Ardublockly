c6ff5f00ccd85728057f7662b4342944edcf01f6
Thiemann96 <e.thieme@reedu.de>
2026-02-10 10:59:40 +0100

    fix: some imports statements

---
+++ src/components/Blockly/toolbox/ToolboxBasic.jsx
@@ -0,0 +1,24 @@
+// src/components/Blockly/toolbox/toolboxBasicPseudo.js
+
+export const toolboxBasicObject = {
+  kind: "flyoutToolbox",
+  contents: [
+    // { kind: "block", type: "bme_basic" },
+    // { kind: "block", type: "bme_tmp" },
+    { kind: "block", type: "hdc_tmp" },
+    { kind: "block", type: "hdc_humi" },
+    { kind: "block", type: "display_print_basic" },
+    { kind: "block", type: "text" },
+    { kind: "block", type: "basic_red" },
+    { kind: "block", type: "basic_blue" },
+    { kind: "block", type: "basic_yellow" },
+    { kind: "block", type: "basic_off" },
+    { kind: "block", type: "time_delay_1s" },
+    { kind: "block", type: "time_delay_2s" },
+    { kind: "block", type: "time_delay_5s" },
+    { kind: "block", type: "basic_if_else" },
+    { kind: "block", type: "basic_repeat_times" },
+    { kind: "block", type: "basic_compare" },
+    { kind: "block", type: "basic_number" },
+  ],
+};
---
+++ src/components/Pages/Basic/BasicPage.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { Box } from "@mui/material";
+import BlocklyCard from "./BlocklyCard";
+import "@/components/Blockly/blocks/basic/index"; // registriert Block
+import senseboxlogo from "@/data/senseBox_Icon_bunt.png";
+import { useSelector } from "react-redux";
+
+const BasicPage = () => {
+  const basicCode = useSelector((s) => s.workspace.code.basic);
+
+  useEffect(() => {
+    const head = document.head;
+    const link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = new URL("./toolbox_style.css", import.meta.url).href;
+    head.appendChild(link);
+
+    // 🎨 Hintergrund setzen (freundlich, senseBox Basic)
+    const root = document.querySelector(":root");
+
+    root.style.setProperty("--url", `url(${senseboxlogo})`);
+
+    // 🧹 Cleanup: CSS entfernen und Hintergrund zurücksetzen
+    return () => {
+      if (head.contains(link)) {
+        head.removeChild(link);
+      }
+      root.style.removeProperty("--url");
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        p: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        height: "80vh",
+        boxSizing: "border-box",
+      }}
+    >
+      <Box sx={{ display: "flex", gap: 2, flex: 1, minHeight: 0 }}>
+        {/* Linke Seite */}
+        <Box sx={{ flex: " 0 0 100%", minHeight: 0, display: "flex" }}>
+          <BlocklyCard />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default BasicPage;
