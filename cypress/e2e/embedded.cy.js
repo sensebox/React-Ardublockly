6d9a620d602d53eabfede3120e0ed2d8c018e324
Maria Zadnepryanets <mariya.z@gmail.com>
2026-04-01 15:37:13 +0200

    fix: another test fix

--- cypress/e2e/embedded.cy.js
+++ cypress/e2e/embedded.cy.js
@@ -103,10 +103,10 @@ describe("Embedded Blockly Page Tests", () => {
     cy.get('img[alt="Sensebox ESP"]', { timeout: 8000 }).click();
     
     cy.get(".blocklySvg", { timeout: 10000 }).should("exist").and("be.visible");
-    cy.get(".blocklyToolboxDiv", { timeout: 10000 }).should("exist");
-    cy.get(".blocklyTreeRoot", { timeout: 10000 }).should("exist");
-    cy.get(".blocklyTreeRow").should("have.length.greaterThan", 0);
-    cy.get(".blocklyTreeRow").first().click();
+    cy.get(".blocklyToolbox", { timeout: 10000 }).should("exist");
+    cy.get(".blocklyToolboxCategoryContainer", { timeout: 10000 }).should("exist");
+    cy.get(".blocklyToolboxCategory").should("have.length.greaterThan", 0);
+    cy.get(".blocklyToolboxCategory").first().click();
     cy.get(".blocklyFlyout", { timeout: 5000 }).should("be.visible");
     cy.get(".blocklyFlyout .blocklyBlockCanvas > .blocklyDraggable", { timeout: 5000 })
       .should("have.length.greaterThan", 0);
