f18cb63e850f18bd2e8bbf2d26b6f1b536502ca7
Thiemann96 <e.thieme@reedu.de>
2026-08-04 15:20:05 +0200

    lint

--- src/components/Pages/Impressum.jsx
+++ src/components/Pages/Impressum.jsx
@@ -129,9 +129,9 @@ class Impressum extends Component {
                 diese fremden Inhalte auch keine Gewähr übernehmen. Für die
                 Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
                 oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
-                wurden zum Zeitpunkt der Verlinkung auf mögliche
-                Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
-                Zeitpunkt der Verlinkung nicht erkennbar.
+                wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+                überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
+                Verlinkung nicht erkennbar.
               </Typography>
               <Typography variant="body1">
                 Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist
