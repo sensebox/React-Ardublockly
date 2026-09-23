bffc1a4c90a76dc62e752a2f356af029118e412c
Thiemann96 <e.thieme@reedu.de>
2026-09-23 15:28:50 +0200

    bump versions, use legacy create store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 import { loadUser, setupInterceptors } from "./actions/authActions";
 
